a55892fdef4067e7a2a0a589e905ccd9d86b197c
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-11-01 19:02:04 +0100

    add device-registry

---
+++ cmd/device-registry/device-registry.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/infinimesh/infinimesh/pkg/registry"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const port = ":8080"
+
+var dbAddr string
+
+func init() {
+	viper.SetDefault("DB_HOST", "localhost")
+	viper.AutomaticEnv()
+
+	dbAddr = viper.GetString("DB_ADDR")
+}
+
+func main() {
+	server := registry.NewServer(dbAddr)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	registry.RegisterDevicesServer(s, server)
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
