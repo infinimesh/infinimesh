b119ebf89d09e33c7ff5a4194023166bea762ca2
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-11-04 20:28:59 +0100

    shadow/api: use Context.Done() instead of CloseNotifier
    CloseNotifier is deprecated

--- cmd/shadow-api/api.go
+++ cmd/shadow-api/api.go
@@ -126,7 +126,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		subMtx.Unlock()
 	}()
 
-	notify := w.(http.CloseNotifier).CloseNotify()
+	notify := r.Context().Done()
 
 outer:
 	for {
