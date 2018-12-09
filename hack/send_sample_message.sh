8e305bbeedbce9111fcfd4c786dde626a039f453
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-12-09 21:19:43 +0100

    improve sample message script to send random data
    if we send the same every thing, it's not helpful as the shadow filters out
    messages that don't change the shadow

--- hack/send_sample_message.sh
+++ hack/send_sample_message.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo '{"sampleKey" : "sampleString"}' | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice3" -broker localhost:8089
+echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice3" -broker localhost:8089
