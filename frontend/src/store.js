a817b0e021d8fcefe342a4f268e4bf3abb013efb
Florian <florian.gruenwald@hec.edu>
2018-12-13 15:34:12 +0100

    state management part1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,47 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    devices: {
+      testid123: {
+        status: "active",
+        name: "Device 3",
+        location: "Berlin",
+        tags: "test"
+      },
+      testid124: {
+        status: "active",
+        name: "Device 4",
+        location: "Ddorf",
+        tags: "test"
+      }
+    }
+  },
+  getters: {
+    getDevice: (state, deviceId) => {
+      return state.devices.deviceId;
+    },
+    getAllDevices: (state) => {
+      return state.devices;
+    }
+  },
+  mutations: {
+    addDevice: (state, device) => {
+      state.devices[Object.keys(device)[0]] = device.deviceId;
+      console.log(device)
+    },
+    deleteDevice: (state, deviceId) => {
+      delete state.devices.deviceId;
+    }
+  },
+  actions: {
+    addDevice: ({ commit }, device) => {
+      commit("addDevice", device);
+      return device;
+    },
+    deleteDevice: ({ commit }, deviceId) => {
+      commit("deleteDevice", deviceId);
+      return deviceId;
+    }
+  }
 });
