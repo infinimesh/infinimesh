de0eea35e9cff0cb1350c7bf97bb8e24e2ba04b2
Florian <florian.gruenwald@hec.edu>
2018-12-14 16:00:28 +0100

    complete state mgmt / create shadow draft

--- frontend/src/views/RegisterDevice.vue
+++ frontend/src/views/RegisterDevice.vue
@@ -107,11 +107,6 @@ export default {
       }
     };
   },
-  computed: {
-    devices() {
-      return this.$store.getters.getAllDevices;
-    }
-  },
   methods: {
     addTag(event) {
       this.tags.push(event.target.value);
