576e7f7060aeee36dc973f3b05548bb8d53f20a2
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-12-18 23:19:19 +0100

    return partial shadow if reported/desired is not set

--- pkg/shadow/server.go
+++ pkg/shadow/server.go
@@ -18,6 +18,15 @@ type Server struct {
 }
 
 func (s *Server) Get(context context.Context, req *shadowpb.GetRequest) (response *shadowpb.GetResponse, err error) {
+	response = &shadowpb.GetResponse{
+		Shadow: &shadowpb.Shadow{},
+	}
+
+	ts, err := ptypes.TimestampProto(time.Now())
+	if err != nil {
+		return nil, err
+	}
+
 	reportedState, err := s.Repo.GetReported(req.Id)
 	if err != nil {
 		return nil, err
@@ -32,31 +41,25 @@ func (s *Server) Get(context context.Context, req *shadowpb.GetRequest) (respons
 
 	var reportedValue structpb.Value
 	if err := u.Unmarshal(bytes.NewReader(reportedState.State), &reportedValue); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to unmarshal JSON from database: %v", err)
+		fmt.Fprintf(os.Stderr, "Failed to unmarshal reported JSON from database: %v\n", err)
+	} else {
+		response.Shadow.Reported = &shadowpb.VersionedValue{
+			Version:   uint64(reportedState.Version),
+			Data:      &reportedValue,
+			Timestamp: ts,
+		}
 	}
 
 	var desiredValue structpb.Value
 	if err := u.Unmarshal(bytes.NewReader(desiredState.State), &desiredValue); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to unmarshal JSON from database: %v", err)
+		fmt.Fprintf(os.Stderr, "Failed to unmarshal JSON from database: %v\n", err)
+	} else {
+		response.Shadow.Desired = &shadowpb.VersionedValue{
+			Version:   uint64(desiredState.Version),
+			Data:      &desiredValue,
+			Timestamp: ts,
+		}
 	}
 
-	ts, err := ptypes.TimestampProto(time.Now())
-	if err != nil {
-		return nil, err
-	}
-
-	return &shadowpb.GetResponse{
-		Shadow: &shadowpb.Shadow{
-			Reported: &shadowpb.VersionedValue{
-				Version:   uint64(reportedState.Version),
-				Data:      &reportedValue,
-				Timestamp: ts,
-			},
-			Desired: &shadowpb.VersionedValue{
-				Version:   uint64(desiredState.Version),
-				Data:      &desiredValue,
-				Timestamp: ts,
-			},
-		},
-	}, nil
+	return
 }
