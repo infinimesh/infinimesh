b393823b58aec69a6b5a2326f1ce2d3ac7f8cd6d
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-12-18 14:37:11 +0100

    shadow: add desired state to repo and api impl

--- pkg/shadow/repository.go
+++ pkg/shadow/repository.go
@@ -11,8 +11,8 @@ import (
 type Repo interface {
 	SetReported(DeviceState) (err error)
 	GetReported(id string) (DeviceState, error)
-	// SetDesired(DeviceState)
-	// GetDesired() (DeviceState, error)
+	SetDesired(DeviceState) (err error)
+	GetDesired(id string) (DeviceState, error)
 }
 
 type DeviceState struct {
@@ -67,5 +67,27 @@ func (p *postgresRepo) GetReported(id string) (DeviceState, error) {
 	return DeviceState{ID: d.ID,
 		Version: d.ReportedVersion,
 		State:   d.ReportedState.RawMessage,
+	}, nil
+}
+
+func (p *postgresRepo) SetDesired(d DeviceState) (err error) {
+	if err := p.db.Save(&DeviceStateDB{
+		ID:             d.ID,
+		DesiredVersion: d.Version,
+		DesiredState:   postgres.Jsonb{d.State}, // nolint
+	}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *postgresRepo) GetDesired(id string) (DeviceState, error) {
+	var d DeviceStateDB
+	if err := p.db.First(&d, "id = ?", id).Error; err != nil {
+		return DeviceState{}, err
+	}
+	return DeviceState{ID: d.ID,
+		Version: d.DesiredVersion,
+		State:   d.DesiredState.RawMessage,
 	}, nil
 }
--- pkg/shadow/server.go
+++ pkg/shadow/server.go
@@ -17,15 +17,25 @@ type Server struct {
 }
 
 func (s *Server) Get(context context.Context, req *shadowpb.GetRequest) (response *shadowpb.GetResponse, err error) {
-	state, err := s.Repo.GetReported(req.Id)
+	reportedState, err := s.Repo.GetReported(req.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	desiredState, err := s.Repo.GetDesired(req.Id)
 	if err != nil {
 		return nil, err
 	}
 
 	u := &jsonpb.Unmarshaler{}
 
-	var value structpb.Value
-	if err := u.Unmarshal(bytes.NewReader(state.State), &value); err != nil {
+	var reportedValue structpb.Value
+	if err := u.Unmarshal(bytes.NewReader(reportedState.State), &reportedValue); err != nil {
+		return nil, errors.New("Failed to unmarshal JSON from database")
+	}
+
+	var desiredValue structpb.Value
+	if err := u.Unmarshal(bytes.NewReader(desiredState.State), &desiredValue); err != nil {
 		return nil, errors.New("Failed to unmarshal JSON from database")
 	}
 
@@ -37,8 +47,13 @@ func (s *Server) Get(context context.Context, req *shadowpb.GetRequest) (respons
 	return &shadowpb.GetResponse{
 		Shadow: &shadowpb.Shadow{
 			Reported: &shadowpb.VersionedValue{
-				Version:   uint64(state.Version),
-				Data:      &value,
+				Version:   uint64(reportedState.Version),
+				Data:      &reportedValue,
+				Timestamp: ts,
+			},
+			Desired: &shadowpb.VersionedValue{
+				Version:   uint64(desiredState.Version),
+				Data:      &desiredValue,
 				Timestamp: ts,
 			},
 		},
