dcb015f0868ba4aa48047f58ec08cefe146b379f
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-12-30 13:36:23 +0100

    auth: use log pkg

--- cmd/authserver/main.go
+++ cmd/authserver/main.go
@@ -11,32 +11,37 @@ import (
 	"github.com/dgraph-io/dgo/protos/api"
 	"github.com/infinimesh/infinimesh/pkg/auth"
 	"github.com/infinimesh/infinimesh/pkg/auth/authpb"
+	"github.com/infinimesh/infinimesh/pkg/log"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 var (
-	dgraphURL = "127.0.0.1:9080"
+	dgraphURL   = "127.0.0.1:9080"
+	grpcAddress = ":8082"
 )
 
 func main() {
-	log, err := zap.NewDevelopment()
+	log, err := log.NewProdOrDev()
 	if err != nil {
 		panic(err)
 	}
+
 	defer log.Sync()
+	log.Info("Connecting to dgraph", zap.String("URL", dgraphURL))
 	conn, err := grpc.Dial(dgraphURL, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal("Failed to connect to dgraph", zap.Error(err))
 	}
+	log.Info("Connected to dgraph")
 	defer conn.Close()
 
 	dg := dgo.NewDgraphClient(api.NewDgraphClient(conn))
 
 	lis, err := net.Listen("tcp", ":8082")
 	if err != nil {
-		log.Fatal("Failed to listen", zap.Error(err))
+		log.Fatal("Failed to listen", zap.String("address", grpcAddress), zap.Error(err))
 	}
 
 	srv := grpc.NewServer()
@@ -52,12 +57,14 @@ func main() {
 	signal.Notify(signals, syscall.SIGINT)
 
 	go func() {
-		log.Info("Starting gRPC server")
+		log.Info("Starting gRPC server", zap.String("address", grpcAddress))
 		if err := srv.Serve(lis); err != nil {
 			log.Fatal("Failed to serve gRPC", zap.Error(err))
 		}
 	}()
 
 	<-signals
-	log.Info("Exiting")
+	log.Info("Stopping gRPC server")
+	srv.GracefulStop()
+	log.Info("Stopped gRPC server")
 }
