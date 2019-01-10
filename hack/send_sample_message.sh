2cac887243ec091830f94df5a9dc014d84d4cf1c
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-01-10 21:45:47 +0100

    hack: add script to send to dev server

--- hack/send_sample_message.sh
+++ hack/send_sample_message.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice3" -broker localhost:8089
+echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice4" -broker localhost:8089
---
+++ hack/send_sample_message_devserver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice4" -broker controlplane.infinimesh.io:8089
