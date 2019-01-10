6a40f0bade96749d860f4e0e25ed86f900d94156
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-01-10 21:44:10 +0100

    Revert "hack: use wildcard common name in example cert"
    This reverts commit 1b1a143ba0454baf733cf5ac9dc2bd37530b2f76.

--- hack/send_sample_message.sh
+++ hack/send_sample_message.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice4" -broker localhost:8089
-#echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice4" -broker controlplane.infinimesh.io:8089
+echo "{\"sampleKey\" : $RANDOM}" | go run hack/mqtt_testclients/produce/produce.go -topic "shadows/testdevice3" -broker localhost:8089
