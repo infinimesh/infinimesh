34e7c0106ed0a4667600ccb637ddc6624b168e87
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-01-13 02:38:01 +0100

    add token endpoint to grpc-gateway

--- pkg/apiserver/apipb/api.pb.gw.go
+++ pkg/apiserver/apipb/api.pb.gw.go
@@ -302,6 +302,23 @@ func request_Shadows_StreamReportedStateChanges_0(ctx context.Context, marshaler
 
 }
 
+var (
+	filter_Account_Token_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Account_Token_0(ctx context.Context, marshaler runtime.Marshaler, client AccountClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq TokenRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Account_Token_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Token(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterDevicesHandlerFromEndpoint is same as RegisterDevicesHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterDevicesHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -606,4 +623,73 @@ var (
 	forward_Shadows_PatchDesiredState_0 = runtime.ForwardResponseMessage
 
 	forward_Shadows_StreamReportedStateChanges_0 = runtime.ForwardResponseStream
+)
+
+// RegisterAccountHandlerFromEndpoint is same as RegisterAccountHandler but
+// automatically dials to "endpoint" and closes the connection when "ctx" gets done.
+func RegisterAccountHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
+	conn, err := grpc.Dial(endpoint, opts...)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+			return
+		}
+		go func() {
+			<-ctx.Done()
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+		}()
+	}()
+
+	return RegisterAccountHandler(ctx, mux, conn)
+}
+
+// RegisterAccountHandler registers the http handlers for service Account to "mux".
+// The handlers forward requests to the grpc endpoint over "conn".
+func RegisterAccountHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return RegisterAccountHandlerClient(ctx, mux, NewAccountClient(conn))
+}
+
+// RegisterAccountHandlerClient registers the http handlers for service Account
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "AccountClient".
+// Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "AccountClient"
+// doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
+// "AccountClient" to call the correct interceptors.
+func RegisterAccountHandlerClient(ctx context.Context, mux *runtime.ServeMux, client AccountClient) error {
+
+	mux.Handle("GET", pattern_Account_Token_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Account_Token_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Account_Token_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	return nil
+}
+
+var (
+	pattern_Account_Token_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"token"}, ""))
+)
+
+var (
+	forward_Account_Token_0 = runtime.ForwardResponseMessage
 )
