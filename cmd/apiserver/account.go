1495d9c6efffdd3b82432b4d908912cd7b2232f8
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-01-14 01:13:31 +0100

    apiserver: check credentials through nodeserver in token endpoint

--- cmd/apiserver/account.go
+++ cmd/apiserver/account.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/infinimesh/infinimesh/pkg/apiserver/apipb"
@@ -18,28 +17,34 @@ type accountAPI struct {
 
 func (a *accountAPI) Token(ctx context.Context, request *apipb.TokenRequest) (response *apipb.TokenResponse, err error) {
 	// TODO check password :D
-	resp, err := a.client.GetAccount(ctx, &nodepb.GetAccountRequest{Name: request.GetUsername()})
+	resp, err := a.client.Authenticate(ctx, &nodepb.AuthenticateRequest{Username: request.GetUsername(), Password: request.GetPassword()})
 	if err != nil {
 		return nil, err
 	}
 
-	// Issue token
-	// Create a new token object, specifying signing method and the claims
-	// you would like it to contain.
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		accountIDClaim: resp.Uid,
-	})
+	if resp.GetSuccess() {
+		if resp.Account == nil {
+			return nil, status.Error(codes.Internal, "Failed to check credentials")
+		}
+		// Issue token
+		// Create a new token object, specifying signing method and the claims
+		// you would like it to contain.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			accountIDClaim: resp.Account.Uid,
+		})
 
-	// Sign and get the complete encoded token as a string using the secret
-	tokenString, err := token.SignedString(a.signingSecret)
-	if err != nil {
-		return nil, status.Error(codes.Internal, "Failed to sign token")
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := token.SignedString(a.signingSecret)
+		if err != nil {
+			return nil, status.Error(codes.Internal, "Failed to sign token")
+		}
+
+		return &apipb.TokenResponse{Token: tokenString}, nil
 	}
 
-	return &apipb.TokenResponse{Token: tokenString}, nil
+	return nil, status.Error(codes.Unauthenticated, "Invalid credentials")
 }
 
 func (a *accountAPI) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
-	fmt.Println("override")
 	return ctx, nil
 }
