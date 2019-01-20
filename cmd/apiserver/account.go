6db9d810e4cbd3690f136fc1c2252427ff0260de
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-01-20 23:05:24 +0100

    remove obsolete comments

--- cmd/apiserver/account.go
+++ cmd/apiserver/account.go
@@ -26,9 +26,7 @@ func (a *accountAPI) Token(ctx context.Context, request *apipb.TokenRequest) (re
 		if resp.Account == nil {
 			return nil, status.Error(codes.Internal, "Failed to check credentials")
 		}
-		// Issue token
-		// Create a new token object, specifying signing method and the claims
-		// you would like it to contain.
+
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 			accountIDClaim: resp.Account.Uid,
 		})
