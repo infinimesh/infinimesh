ffd645fb555baae5c1ee3814bccd6cdac5aa3e9a
Florian <florian.gruenwald@hec.edu>
2019-02-10 14:11:54 +0100

    add sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app>
-    <Header class="my-1"></Header>
+    <Header></Header>
+    <v-layout
+      row
+      wrap
+    >
+        <Sidebar />
+      <v-flex>
         <router-view></router-view>
+      </v-flex>
+    </v-layout>
     <Footer></Footer>
   </v-app>
 </template>
@@ -9,12 +17,14 @@
 <script>
 import Footer from "./components/TheFooter.vue";
 import Header from "./components/TheHeader.vue";
+import Sidebar from "./components/TheSidebar.vue";
 
 export default {
   name: "App",
   components: {
     Footer,
-    Header
+    Header,
+    Sidebar
   },
   data() {
     return {
---
+++ frontend/src/components/TheSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-navigation-drawer
+      stateless
+      value="true"
+    >
+      <v-list>
+        <div
+          v-for="page in pages"
+          :key="page"
+        >
+          <v-list-tile>
+            <v-list-tile-action />
+            <v-list-tile-title>{{ page }}</v-list-tile-title>
+          </v-list-tile>
+          <v-divider></v-divider>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    pages: ["Devices", "Assets", "Accounts", "Monitoring", "Integrations"]
+  })
+};
+</script>
+
+<style lang="css" scoped>
+</style>
