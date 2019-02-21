a91efda643ed82f8de62860d018a275e9b4cb287
alo.alt <alo.alt@me.com>
2019-02-21 15:36:04 +0100

    - add redirect to main page after logout

--- frontend/src/views/LogoutUser.vue
+++ frontend/src/views/LogoutUser.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h1 class="mb-3">Successfully logged out. See you soon!</h1>
+    <span>We redirect you to the login page in 10 seconds ... </span>
     <v-card>
     </v-card>
   </v-container>
@@ -8,11 +9,10 @@
 
 <script>
 export default {
-  data() {
-    return {};
+  created() {
+    setTimeout(() => this.$router.push({ path: "/login" }), 10000);
   }
 };
 </script>
-
 <style scoped>
 </style>
