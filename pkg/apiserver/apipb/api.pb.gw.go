c7c5f72089d02e03788a9485e70f683e8d0004d6
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-02-24 23:52:51 +0100

    api: fix shadow namespace typo

--- pkg/apiserver/apipb/api.pb.gw.go
+++ pkg/apiserver/apipb/api.pb.gw.go
@@ -929,7 +929,7 @@ var (
 
 	pattern_Shadows_PatchDesiredState_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 1, 0, 4, 1, 5, 2, 2, 3, 2, 4}, []string{"namespaces", "namespace", "id", "shadow", "desired"}, ""))
 
-	pattern_Shadows_StreamReportedStateChanges_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 0, 2, 1, 1, 0, 4, 1, 5, 0, 1, 0, 4, 1, 5, 2, 2, 3, 2, 4}, []string{"namespace", "namespaces", "id", "shadow", "reported"}, ""))
+	pattern_Shadows_StreamReportedStateChanges_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 0, 1, 0, 4, 1, 5, 1, 1, 0, 4, 1, 5, 2, 2, 3, 2, 4}, []string{"namespaces", "namespace", "id", "shadow", "reported"}, ""))
 )
 
 var (
