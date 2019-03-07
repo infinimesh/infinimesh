c96a9c3ae2be9389865bb2e8ec53148a989ae3b0
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-03-07 22:28:15 +0100

    fix sample shell scripts

--- hack/read_sample_shadow.sh
+++ hack/read_sample_shadow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TOKEN=$(curl -s -X POST -d '{"username" : "joe", "password": "test123"}'  localhost:8081/token | jq -r ".token")
-grpcurl -H "authorization: bearer $TOKEN" -plaintext -d '{"id" : "testdevice4"}' localhost:8080 infinimesh.api.Shadows/Get | jq
+export TOKEN=$(curl -s -X POST -d '{"username" : "joe", "password": "test123"}'  localhost:8081/accounts/token | jq -r ".token")
+grpcurl -H "authorization: bearer $TOKEN" -plaintext -d '{"id" : "testdevice4", "namespace" : "joe"}' localhost:8080 infinimesh.api.Shadows/Get | jq
