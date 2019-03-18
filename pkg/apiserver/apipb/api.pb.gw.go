e8f7215b11122b5011130118856cbc897096de81
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-03-18 23:01:03 +0100

    twin: remove namespace

--- pkg/apiserver/apipb/api.pb.gw.go
+++ pkg/apiserver/apipb/api.pb.gw.go
@@ -171,10 +171,6 @@ func request_Devices_Delete_0(ctx context.Context, marshaler runtime.Marshaler, 
 
 }
 
-var (
-	filter_Shadows_Get_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
-)
-
 func request_Shadows_Get_0(ctx context.Context, marshaler runtime.Marshaler, client ShadowsClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq shadowpb.GetRequest
 	var metadata runtime.ServerMetadata
@@ -195,10 +191,6 @@ func request_Shadows_Get_0(ctx context.Context, marshaler runtime.Marshaler, cli
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
-	}
-
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Shadows_Get_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.Get(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
