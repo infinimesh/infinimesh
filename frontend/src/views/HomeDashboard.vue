f363de851304ea3adcd6e47ee2c2b8b6f19fd2a2
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-03-21 04:32:05 +0100

    remove deprecated UIs

--- frontend/src/views/HomeDashboard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container>
-    <h1 class="mb-3">Overview</h1>
-    <div id="app">
-      <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23]]"></pie-chart>
-    </div>
-    <v-card> </v-card>
-  </v-container>
-</template>
