5e9e333f8b3493f79bf03783345bb74f480ab7c6
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-03-29 21:21:59 +0100

    enforce authorization in shadow

--- cmd/apiserver/shadow.go
+++ cmd/apiserver/shadow.go
@@ -3,23 +3,79 @@ package main
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/infinimesh/infinimesh/pkg/apiserver/apipb"
+	"github.com/infinimesh/infinimesh/pkg/node/nodepb"
 	"github.com/infinimesh/infinimesh/pkg/shadow/shadowpb"
 )
 
 type shadowAPI struct {
-	client shadowpb.ShadowsClient
+	accountClient nodepb.AccountServiceClient
+	client        shadowpb.ShadowsClient
 }
 
 func (s *shadowAPI) Get(ctx context.Context, request *shadowpb.GetRequest) (response *shadowpb.GetResponse, err error) {
+	account, ok := ctx.Value("account_id").(string)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	resp, err := s.accountClient.IsAuthorized(ctx, &nodepb.IsAuthorizedRequest{
+		Node:    request.Id,
+		Account: account,
+		Action:  nodepb.Action_READ,
+	})
+	if err != nil {
+		return nil, status.Error(codes.PermissionDenied, "Permission denied")
+	}
+	if !resp.GetDecision().GetValue() {
+		return nil, status.Error(codes.PermissionDenied, "Permission denied")
+	}
+
 	return s.client.Get(ctx, request)
 }
 
 func (s *shadowAPI) PatchDesiredState(ctx context.Context, request *shadowpb.PatchDesiredStateRequest) (response *shadowpb.PatchDesiredStateResponse, err error) {
+	account, ok := ctx.Value("account_id").(string)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	resp, err := s.accountClient.IsAuthorized(ctx, &nodepb.IsAuthorizedRequest{
+		Node:    request.Id,
+		Account: account,
+		Action:  nodepb.Action_READ,
+	})
+	if err != nil {
+		return nil, status.Error(codes.PermissionDenied, "Permission denied")
+	}
+	if !resp.GetDecision().GetValue() {
+		return nil, status.Error(codes.PermissionDenied, "Permission denied")
+	}
+
 	return s.client.PatchDesiredState(ctx, request)
 }
 
 func (s *shadowAPI) StreamReportedStateChanges(request *shadowpb.StreamReportedStateChangesRequest, srv apipb.Shadows_StreamReportedStateChangesServer) (err error) {
+	account, ok := srv.Context().Value("account_id").(string)
+	if !ok {
+		return status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	resp, err := s.accountClient.IsAuthorized(context.TODO(), &nodepb.IsAuthorizedRequest{
+		Node:    request.Id,
+		Account: account,
+		Action:  nodepb.Action_READ,
+	})
+	if err != nil {
+		return status.Error(codes.PermissionDenied, "Permission denied")
+	}
+	if !resp.GetDecision().GetValue() {
+		return status.Error(codes.PermissionDenied, "Permission denied")
+	}
+
 	c, err := s.client.StreamReportedStateChanges(context.Background(), request)
 	if err != nil {
 		return err
