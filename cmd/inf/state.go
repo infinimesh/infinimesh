bdc8f0d7b1a8b0ef3c3f05d06d474e4733d98cf8
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-04-07 01:21:39 +0200

    cli: add --watch/-w flag to state get

--- cmd/inf/state.go
+++ cmd/inf/state.go
@@ -21,10 +21,16 @@ import (
 	_struct "github.com/golang/protobuf/ptypes/struct"
 )
 
+var (
+	watch bool
+)
+
 func init() {
 	stateCmd.AddCommand(stateGetCmd)
 	stateCmd.AddCommand(stateSetCmd)
 	rootCmd.AddCommand(stateCmd)
+
+	stateGetCmd.Flags().BoolVarP(&watch, "watch", "w", false, "Watch for changes")
 }
 
 var stateCmd = &cobra.Command{
@@ -83,6 +89,36 @@ var stateGetCmd = &cobra.Command{
 
 		fmt.Fprintf(w, "Configuration:")
 		printState(w, response.Shadow.Config)
+
+		if watch {
+			resp, err := shadowClient.StreamReportedStateChanges(ctx, &shadowpb.StreamReportedStateChangesRequest{
+				Id: args[0],
+			})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to get state: %v\n", err)
+				os.Exit(1)
+			}
+
+			for {
+				msg, err := resp.Recv()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Failed to get state: %v\n", err)
+					os.Exit(1)
+				}
+				if msg.ReportedState != nil {
+					fmt.Fprintf(w, "Reported State:")
+					printState(w, msg.ReportedState)
+				}
+
+				if msg.DesiredState != nil {
+					fmt.Fprintf(w, "Desired State:")
+					printState(w, msg.DesiredState)
+				}
+
+				w.Flush()
+			}
+
+		}
 	},
 }
 
