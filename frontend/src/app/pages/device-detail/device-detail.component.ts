13d1dde02a6d1926f0b97627f986dce9c063ba4e
Dennis Nobel <dennis@dnobel.de>
2019-04-07 11:02:55 +0200

    Frontend: Added desired state to device detail view.

--- frontend/src/app/pages/device-detail/device-detail.component.ts
+++ frontend/src/app/pages/device-detail/device-detail.component.ts
@@ -12,7 +12,8 @@ export class DeviceDetailComponent implements OnInit, OnDestroy {
   device$;
   state;
   stateSubscription;
-  stateUpdated = false;
+  desiredStateUpdated = false;
+  reportedStateUpdated = false;
   JSON = JSON;
 
   constructor(private route: ActivatedRoute,
@@ -27,22 +28,32 @@ export class DeviceDetailComponent implements OnInit, OnDestroy {
           this.state = state;
         } else {
           this.state = {
-            reported: {}
+            reported: {},
+            desired: {}
           };
         }
         this.stateSubscription = this.deviceService.streamState(deviceId).subscribe((data) => {
-          if (data.reportedState) {
-            this.state.reported.data = data.reportedState.data;
-            this.state.reported.timestamp = data.reportedState.timestamp;
-            this.state.reported.version = data.reportedState.version;
-            this.stateUpdated = true
-          }
-          setTimeout(() => {
-            this.stateUpdated = false
-          }, 500);
+          this.handleStateUpdate(data);
         });
       })
     })
+  }
+
+  private handleStateUpdate(data) {
+    if (data.reportedState && data.reportedState !== null) {
+      this.state.reported = data.reportedState;
+      this.reportedStateUpdated = true
+      setTimeout(() => {
+        this.reportedStateUpdated = false
+      }, 500);
+    }
+    if (data.desiredState && data.desiredState !== null) {
+      this.state.desired = data.desiredState;
+      this.desiredStateUpdated = true
+      setTimeout(() => {
+        this.desiredStateUpdated = false
+      }, 500);
+    }
   }
 
   ngOnDestroy(): void {
