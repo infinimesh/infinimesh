14a8f1dda6728714e89168ca829cad060e358ef5
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-04-10 01:54:46 +0200

    timeseries: fix ddl import

--- pkg/timeseries/repo_test.go
+++ pkg/timeseries/repo_test.go
@@ -2,9 +2,13 @@ package timeseries
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"os"
 	"testing"
 	"time"
+
+	"io/ioutil"
 
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
@@ -28,6 +32,25 @@ func init() {
 		panic(err)
 	}
 	repo = r
+
+	conn, err := sql.Open("postgres", dbAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	ddl, err := ioutil.ReadFile("./ddl/ddl.sql")
+	if err != nil {
+		panic(err)
+	}
+
+	txn, err := conn.Begin()
+	if err != nil {
+		panic(err)
+	}
+	_, err = txn.Exec(string(ddl))
+	if err != nil {
+		fmt.Printf("Error during DDL import: %v. Ignoring\n", err)
+	}
 }
 
 func TestSave(t *testing.T) {
