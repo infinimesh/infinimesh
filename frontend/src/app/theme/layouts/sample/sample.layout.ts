b53d231ff7a38e77b44bdc6d869f45fed677d5e2
Dennis Nobel <dennis@dnobel.de>
2019-04-14 14:47:33 +0200

    Frontend: Changed login text, fixed unsubscription and removed unneeded code from layout.

--- frontend/src/app/theme/layouts/sample/sample.layout.ts
+++ frontend/src/app/theme/layouts/sample/sample.layout.ts
@@ -38,40 +38,11 @@ import {
 })
 export class SampleLayoutComponent implements OnDestroy {
 
-  subMenu: NbMenuItem[] = [
-    {
-      title: 'PAGE LEVEL MENU',
-    },
-  ];
-  layout: any = {};
   sidebar: any = {};
-  currentTheme: string;
   private alive = true;
 
-  constructor(protected menuService: NbMenuService,
-              protected themeService: NbThemeService,
-              protected bpService: NbMediaBreakpointsService,
-              protected sidebarService: NbSidebarService) {
+  constructor() {
 
-    const isBp = this.bpService.getByName('is');
-    this.menuService.onItemSelect()
-      .pipe(
-        takeWhile(() => this.alive),
-        withLatestFrom(this.themeService.onMediaQueryChange()),
-        delay(20),
-      )
-      .subscribe(([item, [bpFrom, bpTo]]: [any, [NbMediaBreakpoint, NbMediaBreakpoint]]) => {
-
-        if (bpTo.width <= isBp.width) {
-          this.sidebarService.collapse('menu-sidebar');
-        }
-      });
-
-    this.themeService.getJsTheme()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(theme => {
-        this.currentTheme = theme.name;
-      });
   }
 
   ngOnDestroy() {
