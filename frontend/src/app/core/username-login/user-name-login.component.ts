de420c9ff4339729e7b5af2736dccd9e42d427e1
Dennis Nobel <dennis@dnobel.de>
2019-04-24 20:37:25 +0200

    Frontend: Made register link dependent on SaaS environment.

--- frontend/src/app/core/username-login/user-name-login.component.ts
+++ frontend/src/app/core/username-login/user-name-login.component.ts
@@ -1,9 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NbLoginComponent} from '@nebular/auth';
+import {environment} from '../../../environments/environment';
+
+const apiServerUrl = environment.apiServerUrl;
 
 @Component({
   selector: 'user-name-login',
   templateUrl: './user-name-login.component.html',
 })
-export class UserNameLoginComponent extends NbLoginComponent {
+export class UserNameLoginComponent extends NbLoginComponent implements OnInit {
+
+  public showRegisterLink = false;
+
+  ngOnInit(): void {
+    if (apiServerUrl.includes('api.infinimesh.io')) {
+      this.showRegisterLink = true;
+    }
+  }
 }
