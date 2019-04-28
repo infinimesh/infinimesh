dbb199fcdb808f75ef14da2260151b62ef38a7ee
alo.alt <alo.alt@me.com>
2019-04-28 14:08:37 +0200

    microk8s setup

--- hack/microk8s/README.md
+++ hack/microk8s/README.md
@@ -4,6 +4,11 @@ This script installs infinimesh and microk8s to allow a seamless developer exper
 multipass
 kubectl
 ```
-The script installs microk8s in his own VM, enable all services infinimesh needs + kubernetes Grafana monitoring and infinimesh as a local development system. Please read our documention https://infinimesh.github.io/infinimesh/docs/#/ and our blog https://blog.infinimesh.io/ for latest insights and HowTos.
+## Quickstart
+```
+bash <(curl -s https://raw.githubusercontent.com/infinimesh/infinimesh/master/hack/microk8s/infinimesh-setup.sh)
+```
+
+The script installs microk8s in his own VM, enables DNS, Storage, Ingress + Grafana monitoring and infinimesh as a local development system. Please read our documention https://infinimesh.github.io/infinimesh/docs/#/ and our blog https://blog.infinimesh.io/ for latest insights and HowTos.
 
 Thank you for using infinimesh, you rock!
--- hack/microk8s/infinimesh-setup.sh
+++ hack/microk8s/infinimesh-setup.sh
@@ -16,32 +16,34 @@ fi
 
 # check if multipass is installed
  if which multipass >/dev/null; then
-        echo "multipass found, proceed .."
+        echo " multipass found, proceed .."
     else
-        echo "multipass not found, please install:" \
-              "https://github.com/CanonicalLtd/multipass/releases"
+        echo " multipass not found, please install:" \
+              " https://github.com/CanonicalLtd/multipass/releases"
         exit 0
  fi
 
 # check if kubectl is installed
 if which kubectl >/dev/null; then
-        echo "kubectl found, proceed .."
+        echo " kubectl found, proceed .."
     else
-        echo "kubectl not found, please install:" \
-        echo "for OS X: brew install kubectl" \
-	echo "for Ubuntu: sudo snap install kubectl --classic"
+        echo " kubectl not found, please install:" \
+        echo " for OS X: brew install kubectl" \
+	echo " for Ubuntu: sudo snap install kubectl --classic"
         exit 0
  fi
 
-echo "everything ready, let's start"
+echo "=> everything ready, let's start"
 printf '\n'
 # setup vm and install microk8s
 echo "setup VM and install microk8s into ..." 
-
+printf '\n'
 multipass launch --name microk8s-vm --mem 6G --disk 40G -c 2 &&
 sleep 10
 
 multipass exec microk8s-vm -- sudo snap install microk8s --classic 
+sleep 5
+
 multipass exec microk8s-vm -- sudo iptables -P FORWARD ACCEPT
 sleep 15
 
@@ -56,6 +58,15 @@ sleep 3
 
 multipass exec microk8s-vm -- /snap/bin/microk8s.config > ~/kubeconfig 
 export KUBECONFIG=$KUBECONFIG:~/kubeconfig
+
+# setup kubectl
+if ! grep -q KUBECONFIG "~/.bashrc"; then
+ 	cat 'export KUBECONFIG=$KUBECONFIG:~/kubeconfig' >> ~/.bashrc && . ~/.bashrc
+     else
+  echo " KUBECONFIG set, ignoring ..."
+fi
+printf '\n'
+
 multipass list &&
 printf '\n'
 
@@ -67,31 +78,31 @@ printf '\n'
 	exit 0
  fi
 	 	
-echo "installing infinimesh operator "
+echo " installing infinimesh operator "
 printf '\n'
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/operator/master/manifests/crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/operator/master/manifests/operator.yaml
 sleep 2
 
-echo "installing kubeDB from https://github.com/kubedb "
+echo " installing kubeDB from https://github.com/kubedb "
 printf '\n'
 curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.11.0/hack/deploy/kubedb.sh | bash
 sleep 5
 
-echo "installing Kafka from https://github.com/strimzi "
+echo " installing Kafka from https://github.com/strimzi "
 printf '\n'
 kubectl create namespace kafka &&
 curl -L https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.11.1/strimzi-cluster-operator-0.11.1.yaml \
   | sed 's/namespace: .*/namespace: kafka/' \
   | kubectl -n kafka apply -f -
 printf '\n'
-echo "now we install infinimesh ..."
+echo "=> now we install infinimesh ..."
 printf '\n'
 
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/infinimesh/master/hack/microk8s/infinimesh-platform.yaml
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/infinimesh/master/hack/microk8s/infinimesh-kafka.yaml -n kafka
 
-echo "please note that can take a while, we proceed with self - signed certificates "
+echo " creating self - signed certificates "
 printf '\n'
 mkdir -p certs && cd certs
 openssl genrsa -out ca.key 4096
@@ -110,7 +121,7 @@ openssl req -subj /CN=app.infinimesh.local -out app.csr -key app.key -new
 openssl x509 -req -days 3650 -in app.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out app.crt -sha256
 sleep 3
 
-echo "populate the certificates into kubernetes"
+echo " install the certificates"
 printf '\n'
 kubectl create secret tls apiserver-grpc-tls --cert apiserver_grpc.crt --key apiserver_grpc.key 
 kubectl create secret tls apiserver-rest-tls --cert apiserver_rest.crt --key apiserver_rest.key 
@@ -118,12 +129,12 @@ kubectl create secret tls mqtt-bridge-tls --cert mqtt_bridge.crt --key mqtt_brid
 kubectl create secret tls app-tls --cert app.crt --key app.key 
 
 # getting IP and add hosts entries
-echo "checking for host entries"
+echo " checking for host entries"
 printf '\n'
 
 IP=`multipass list|grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
 if ! grep -q infinimesh.local "/etc/hosts"; then
-echo "please add this host entries into /etc/hosts: "
+echo "=> please add this host entries into /etc/hosts: "
 printf '\n'
 cat <<EOL
 # infinimesh-local
@@ -133,11 +144,11 @@ $IP app.infinimesh.local
 $IP mqtt.api.infinimesh.local
 EOL
    else
-        echo "host entries already present"
+        echo "=> host entries already present"
 fi
 printf '\n'
 
-echo "installing inf - infinimesh command line tool and point to the local setup:"
+echo "=> installing inf (infinimesh CLI) and point to the local setup:"
 curl -L https://bit.ly/2CNKWzJ | BINDIR=$HOME/bin bash  
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.profile && . ~/.profile  
 inf config set-context local --apiserver grpc.api.infinimesh.local:443 --tls=true --ca-file infinimesh-local/certs/ca.crt
