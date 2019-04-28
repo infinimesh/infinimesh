7d43ff7b49a01c7ec2d4dad921362d104d2309c5
alo.alt <alo.alt@me.com>
2019-04-28 13:18:52 +0200

    microk8s setup

--- hack/microk8s/infinimesh-setup.sh
+++ hack/microk8s/infinimesh-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+clear
 
 # create infinimesh directory
 mkdir -p ~/infinimesh-local && cd ~/infinimesh-local
@@ -33,20 +34,27 @@ if which kubectl >/dev/null; then
  fi
 
 echo "everything ready, let's start"
+printf '\n'
 # setup vm and install microk8s
 echo "setup VM and install microk8s into ..." 
 
 multipass launch --name microk8s-vm --mem 6G --disk 40G -c 2 &&
-multipass exec microk8s-vm -- sudo snap install microk8s --classic &&
-multipass exec microk8s-vm -- sudo iptables -P FORWARD ACCEPT
+sleep 10
 
-multipass exec microk8s-vm -- /snap/bin/microk8s.enable dns dashboard &&
+multipass exec microk8s-vm -- sudo snap install microk8s --classic 
+multipass exec microk8s-vm -- sudo iptables -P FORWARD ACCEPT
+sleep 15
+
+multipass exec microk8s-vm -- /snap/bin/microk8s.enable dns dashboard
 sleep 5
-multipass exec microk8s-vm -- /snap/bin/microk8s.enable storage &&
+
+multipass exec microk8s-vm -- /snap/bin/microk8s.enable storage
 sleep 10 
+
 multipass exec microk8s-vm -- /snap/bin/microk8s.enable dns ingress
 sleep 3
-multipass exec microk8s-vm -- /snap/bin/microk8s.config > ~/kubeconfig &&
+
+multipass exec microk8s-vm -- /snap/bin/microk8s.config > ~/kubeconfig 
 export KUBECONFIG=$KUBECONFIG:~/kubeconfig
 multipass list &&
 printf '\n'
@@ -59,26 +67,32 @@ printf '\n'
 	exit 0
  fi
 	 	
-echo "installing infinimesh operator \n"
+echo "installing infinimesh operator "
+printf '\n'
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/operator/master/manifests/crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/operator/master/manifests/operator.yaml
 sleep 2
 
-echo "installing kubeDB from https://github.com/kubedb \n"
+echo "installing kubeDB from https://github.com/kubedb "
+printf '\n'
 curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.11.0/hack/deploy/kubedb.sh | bash
 sleep 5
 
-echo "installing Kafka from https://github.com/strimzi \n"
+echo "installing Kafka from https://github.com/strimzi "
+printf '\n'
 kubectl create namespace kafka &&
 curl -L https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.11.1/strimzi-cluster-operator-0.11.1.yaml \
   | sed 's/namespace: .*/namespace: kafka/' \
   | kubectl -n kafka apply -f -
-echo "please note that can take a while, we proceed with self - signed certificates \n"
-
+printf '\n'
 echo "now we install infinimesh ..."
+printf '\n'
+
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/infinimesh/master/hack/microk8s/infinimesh-platform.yaml
 kubectl apply -f https://raw.githubusercontent.com/infinimesh/infinimesh/master/hack/microk8s/infinimesh-kafka.yaml -n kafka
 
+echo "please note that can take a while, we proceed with self - signed certificates "
+printf '\n'
 mkdir -p certs && cd certs
 openssl genrsa -out ca.key 4096
 openssl req -subj '/CN=infinimesh.local/O=Infinimesh' -new -x509 -sha256 -key ca.key -out ca.crt -days 3650
@@ -96,7 +110,8 @@ openssl req -subj /CN=app.infinimesh.local -out app.csr -key app.key -new
 openssl x509 -req -days 3650 -in app.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out app.crt -sha256
 sleep 3
 
-echo "now we populate the certificates into kubernetes \n"
+echo "populate the certificates into kubernetes"
+printf '\n'
 kubectl create secret tls apiserver-grpc-tls --cert apiserver_grpc.crt --key apiserver_grpc.key 
 kubectl create secret tls apiserver-rest-tls --cert apiserver_rest.crt --key apiserver_rest.key 
 kubectl create secret tls mqtt-bridge-tls --cert mqtt_bridge.crt --key mqtt_bridge.key 
@@ -106,6 +121,7 @@ kubectl create secret tls app-tls --cert app.crt --key app.key
 IP=`multipass list|grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
 if ! grep -q infinimesh.local "/etc/hosts"; then
 echo "please add this host entries into /etc/hosts: "
+printf '\n'
 cat <<EOL
 # infinimesh-local
 $IP grpc.api.infinimesh.local
@@ -115,18 +131,18 @@ $IP mqtt.api.infinimesh.local
 EOL
    else
         echo "host entries already present"
-        exit 0
 fi
+printf '\n'
 
 echo "installing inf - infinimesh command line tool and point to the local setup:"
 curl -L https://bit.ly/2CNKWzJ | BINDIR=$HOME/bin bash  
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.profile && . ~/.profile  
-inf config set-context local --apiserver grpc.api.infinimesh.local:443 --tls=true --ca-file certs/ca.crt
+inf config set-context local --apiserver grpc.api.infinimesh.local:443 --tls=true --ca-file infinimesh-local/certs/ca.crt
 
 echo "infinimesh is now ready, point your browser to app.infinimesh.local or use our cli tool"
 echo "your master user credentials are root /"
 kubectl get secret my-infinimesh-root-account -o=jsonpath='{.data.password}' | base64 -D
-prinf '\n'
+printf '\n'
 echo "To trust the root certificate, you must go to your browser settings and add the file ca.crt as an certificate Authority. This works best with Firefox or Safari, we encounter some issues with Chrome."
 
 echo "please read our documention under https://infinimesh.github.io/infinimesh/docs/#/ to proceed with adding devices and sending data. Happy IoTing ..."
