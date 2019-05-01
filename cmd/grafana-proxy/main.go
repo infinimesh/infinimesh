98246eafee3e66d01675f912f4d965cd51491df3
Johannes Brüderl <johannes.bruederl@gmail.com>
2019-05-02 01:09:53 +0200

    add grafana proxy

---
+++ cmd/grafana-proxy/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	cache "github.com/patrickmn/go-cache"
+
+	"github.com/infinimesh/infinimesh/pkg/node/nodepb"
+)
+
+var (
+	signingSecret  []byte
+	accountIDClaim = "account_id"
+
+	nodeserver string
+
+	log *zap.Logger
+
+	accountService nodepb.AccountServiceClient
+
+	userCache *cache.Cache
+)
+
+func init() {
+	log, _ = zap.NewDevelopment()
+
+	viper.SetDefault("NODE_HOST", "localhost:8082")
+	viper.AutomaticEnv()
+
+	nodeserver = viper.GetString("NODE_HOST")
+
+	{
+		b64SignSecret := viper.GetString("JWT_SIGNING_KEY")
+		if b64SignSecret == "" {
+			panic("Invalid signing secret")
+		}
+		s, err := base64.StdEncoding.DecodeString(b64SignSecret)
+		if err != nil {
+			panic("Failed to base64 decode sign secret")
+		}
+		signingSecret = s
+	}
+
+	userCache = cache.New(1*time.Second, 5*time.Second)
+}
+
+func main() {
+	u, err := url.Parse("http://localhost:3000")
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(u)
+
+	c, err := grpc.Dial(nodeserver, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	accountService = nodepb.NewAccountServiceClient(c)
+
+	http.HandleFunc("/", handler(proxy))
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("token")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		userID, err := ParseJWT(cookie.Value)
+		if err != nil {
+			log.Error("Failed")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		var name string
+		if cachedUser, ok := userCache.Get(userID); ok {
+			if userString, ok := cachedUser.(string); ok {
+				log.Debug("Using cached value", zap.String("userID", userID), zap.String("username", userString))
+				name = userString
+			}
+		} else {
+			acc, err := accountService.GetAccount(context.Background(), &nodepb.GetAccountRequest{
+				Id: userID,
+			})
+			if err != nil {
+				log.Error("Failed to fetch account", zap.String("userID", userID), zap.Error(err))
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			name = acc.Name
+
+			userCache.Add(userID, acc.Name, time.Minute*1)
+		}
+
+		r.Header.Set("X-WEBAUTH-USER", name)
+		p.ServeHTTP(w, r)
+	}
+}
+
+func ParseJWT(tokenString string) (user string, err error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, status.Error(codes.Unauthenticated, fmt.Sprintf("Unexpected signing method: %v", t.Header["alg"]))
+		}
+		return signingSecret, nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", errors.New("Invalid token")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		log.Info("Validated token", zap.Any("claims", claims))
+
+		if accountID, ok := claims[accountIDClaim]; ok {
+			if accountIDStr, ok := accountID.(string); ok {
+				return accountIDStr, nil
+			}
+
+		}
+		log.Info("Token does not contain account id field", zap.Any("token", token))
+	}
+
+	return "", errors.New("Invalid token")
+
+}
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
 	github.com/avast/retry-go v2.2.0+incompatible
 	github.com/birdayz/conjungo v1.0.1-0.20181127234741-4eb7246db343
-	github.com/cskr/pubsub v1.0.1
+	github.com/cskr/pubsub v1.0.2
 	github.com/dgraph-io/dgo v0.0.0-20181226155115-a20f509ade83
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/eapache/go-resiliency v1.1.0 // indirect
@@ -26,7 +26,6 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/grpc-ecosystem/grpc-gateway v1.7.0
 	github.com/hokaccha/go-prettyjson v0.0.0-20180920040306-f579f869bbfe
-	github.com/imdario/mergo v0.3.7
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/infinimesh/mqtt-go v0.0.0-20190331134856-46f3ca3950fb
 	github.com/jeremywohl/flatten v0.0.0-20180923035001-588fe0d4c603
@@ -36,6 +35,7 @@ require (
 	github.com/manifoldco/promptui v0.3.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/nicksnyder/go-i18n v1.10.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/pkg/errors v0.8.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20181016184325-3113b8401b8a // indirect
