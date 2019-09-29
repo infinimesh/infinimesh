055311f334d92e0ea9fafcd533828311b5ffb22f
2pk03 <alo.alt@me.com>
2019-09-29 15:34:26 +0200

    v1.13

--- hack/microk8s/infinimesh-setup-osx.sh
+++ hack/microk8s/infinimesh-setup-osx.sh
@@ -41,7 +41,7 @@ printf '\n'
 multipass launch --name infinimesh-dev --mem 3G --disk 5G -c 3 &&
 sleep 10
 
-multipass exec infinimesh-dev -- sudo snap install microk8s --classic --channel=1.14/stable
+multipass exec infinimesh-dev -- sudo snap install microk8s --classic --channel=1.13/stable
 sleep 5
 
 multipass exec infinimesh-dev -- sudo iptables -P FORWARD ACCEPT
