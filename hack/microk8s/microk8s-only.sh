65f217c66d768e58c76d0f5782859e2e7ff5ed24
2pk03 <alo.alt@me.com>
2019-10-05 13:42:24 +0200

    kubeconfig cleanup

--- hack/microk8s/microk8s-only.sh
+++ hack/microk8s/microk8s-only.sh
@@ -20,21 +20,13 @@ sleep 30
 /snap/bin/microk8s.enable storage
 sleep 10
 
-/snap/bin/microk8s.config > ~/kubeconfig 
-export KUBECONFIG=$KUBECONFIG:~/kubeconfig
+mkdir -p ${HOME}/.kube
+/snap/bin/microk8s.config > ${HOME}/.kube/config
 
 # retrieve token
 token=$(kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
 echo `kubectl -n kube-system describe secret $token` > .k8stoken
 kubectl -n kube-system describe secret $token
-
-# setup kubectl
-if ! grep -q KUBECONFIG ~/.bashrc; then
- 	echo "export KUBECONFIG=$KUBECONFIG:~/kubeconfig" >> ~/.bashrc
-     else
-  echo " KUBECONFIG set, ignoring ..."
-fi
-printf '\n'
 
 # check if we can reach kubernetes and abort if not
  if kubectl cluster-info; then
@@ -51,3 +43,5 @@ sleep 15
 
 kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 sleep 10
+
+echo " done"
