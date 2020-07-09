fbbaa39961d3fba8a52485351587b7fdfe4b8edd
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-09 15:40:51 +0200

    Base Nuxt.js App

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'ngx-dashboard',
-  templateUrl: './dashboard.component.html',
-})
-export class DashboardComponent {
-}
--- frontend/src/app/theme/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './header/header.component';
-export * from './footer/footer.component';
