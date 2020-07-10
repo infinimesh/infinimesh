00068a5625ba6336e57f9665068d78f08e497e63
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-10 15:58:22 +0200

    base login routing middleware

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <app-logo />
       <h1 class="title">
         console.infinimesh.app
       </h1>
@@ -9,27 +9,29 @@
         Console Infinimesh UI
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey"
+          >GitHub</a
+        >
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
-    AppLogo
-  }
-}
+    AppLogo,
+  },
+  middleware: "auth",
+};
 </script>
 
 <style>
@@ -42,7 +44,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
@@ -62,4 +65,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-
---
+++ frontend/pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <a-row type="flex" justify="center" class="rootRow" align="middle">
+    <a-col :span="12">
+      <a-row>
+        <h1>Login</h1>
+      </a-row>
+      <a-row>
+        <a-form>
+          <a-form-item></a-form-item>
+        </a-form>
+      </a-row>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface IUser {
+  login: string;
+  password: string;
+}
+
+export default Vue.extend({
+  data() {
+    const form = {};
+    return {
+      form
+    };
+  }
+});
+</script>
+
+<style scoped>
+.rootRow {
+  height: 100%;
+  min-height: 500px;
+  width: 100%;
+}
+</style>
