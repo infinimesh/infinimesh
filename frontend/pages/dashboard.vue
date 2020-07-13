962534b106d03f7bc921e4e423ec5d692cfb168c
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-13 13:17:06 +0200

    working Authorization Form and middleware

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -2,35 +2,23 @@
   <section class="container">
     <div>
       <app-logo />
-      <h1 class="title">
-        console.infinimesh.app
-      </h1>
-      <h2 class="subtitle">
-        Console Infinimesh UI
-      </h2>
+      <h1 class="title">console.infinimesh.app</h1>
+      <h2 class="subtitle">Console Infinimesh UI</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue";
+import AppLogo from "@/components/AppLogo.vue";
 
 export default {
   components: {
-    AppLogo,
-  },
-  middleware: "auth",
+    AppLogo
+  }
 };
 </script>
 
