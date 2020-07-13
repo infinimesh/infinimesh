dcd81866cab99442060eb53b85b7fe6a919a5df6
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-14 00:49:44 +0200

    Nav bar pre-final layout

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <a-row class="gay-theme-nav">
-    <a-col :span="1">
+    <a-col :xs="{ span: 1, offset: 1 }" :md="{ span: 1, offset: 0 }">
       <a @click="toggleCollapsed" class="menu-control">
-        <a-icon :type="value ? 'menu-unfold' : 'menu-fold'" />
+        <a-icon :type="value ? 'menu-fold' : 'menu-unfold'" />
       </a>
     </a-col>
 
-    <a-col :span="6">
+    <a-col :xs="{ span: 12, offset: 2 }" :sm="{ offset: 1 }" :md="{ span: 8 }">
       <a-row type="flex" justify="start">
         <a-col>
           <img
@@ -18,7 +18,14 @@
         <a-col class="logo">infinimesh<span>.io</span></a-col>
       </a-row>
     </a-col>
-    <a-col :span="3" :offset="10">
+    <a-col
+      :xs="{ span: 6, offset: 1 }"
+      :sm="{ span: 4, offset: 4 }"
+      :md="{ span: 4, offset: 4 }"
+      :lg="{ span: 4, offset: 4 }"
+      :xl="{ span: 6, offset: 4 }"
+      :xxl="{ span: 3, offset: 7 }"
+    >
       <a-select
         style="width: 100%"
         :default-value="$store.state.devices.namespace"
@@ -27,6 +34,25 @@
           >NS: {{ ns.name }}</a-select-option
         >
       </a-select>
+    </a-col>
+    <a-col
+      :xs="{ span: 0 }"
+      :md="{ span: 5, offset: 1 }"
+      :xl="{ span: 3, offset: 1 }"
+    >
+      <a-row type="flex" justify="end">
+        <a-col :span="8">
+          <a-avatar>R</a-avatar>
+        </a-col>
+        <a-col
+          class="user"
+          :xs="{ span: 0 }"
+          :sm="{ span: 0 }"
+          :md="{ span: 16 }"
+        >
+          root
+        </a-col>
+      </a-row>
     </a-col>
   </a-row>
 </template>
@@ -60,11 +86,34 @@ img.logo {
 }
 div.logo {
   padding: 0 1.25rem;
-  font-size: 1.75rem;
   font-weight: 500;
   white-space: nowrap;
   font-family: Exo;
   color: white;
+}
+@media screen and (max-width: 448px) {
+  div.logo {
+    font-size: 1.2rem;
+    padding: 0;
+    padding-left: 0.3rem;
+  }
+  img.logo {
+    height: 30px;
+  }
+}
+@media screen and (min-width: 448px) {
+  div.logo {
+    font-size: 1.5rem;
+  }
+}
+@media screen and (min-width: 576px) {
+  div.logo {
+    font-size: 1.75rem;
+  }
+}
+div.user {
+  color: white;
+  text-align: left;
 }
 .root {
   box-shadow: 0 8px 20px 0 rgba(40, 37, 89, 0.6);
