98b90e8776b9b3a33af9f457aeec0fd9e7c33e61
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-14 15:14:51 +0200

    proto-Sider

---
+++ frontend/components/Sider.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-menu theme="dark" mode="inline" v-model="route">
+    <a-menu-item key="dashboard">
+      <a-icon type="cloud" />
+      <span>Device Registry</span>
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<script>
+export default {
+  computed: {
+    route: {
+      get() {
+        return [this.$route.name];
+      }
+    }
+  }
+};
+</script>
