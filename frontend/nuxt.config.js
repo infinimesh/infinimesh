5ca41f5ad94918e30dfa16566367d0f0d9b1deeb
Mikita Iwanowski <info@slnt-opp.xyz>
2020-08-07 10:26:49 +0200

    probable memory leak fix

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -49,8 +49,7 @@ module.exports = {
   modules: ["@nuxtjs/axios", "@nuxtjs/auth"],
 
   axios: {
-    proxy: true,
-    browserBaseURL: "/api/"
+    proxy: true
   },
   proxy: {
     "/api": {
@@ -65,7 +64,7 @@ module.exports = {
       local: {
         endpoints: {
           login: {
-            url: "account/token",
+            url: "api/account/token",
             method: "post",
             propertyName: "token"
           },
