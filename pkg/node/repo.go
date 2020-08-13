06a888f365a3686785bfe6418c9f44254b7b4af1
Ankit Trivedi <ankittrivedi@MacBook-Pro.local>
2020-08-13 14:54:51 +0200

    IN-26 Creaet the GetNamespaceID function to get ID

--- pkg/node/repo.go
+++ pkg/node/repo.go
@@ -47,6 +47,7 @@ type Repo interface {
 
 	CreateNamespace(ctx context.Context, name string) (id string, err error)
 	GetNamespace(ctx context.Context, uid string) (namespace *nodepb.Namespace, err error)
+	GetNamespaceID(ctx context.Context, uid string) (namespace *nodepb.Namespace, err error)
 	ListNamespaces(ctx context.Context) (namespaces []*nodepb.Namespace, err error)
 	ListNamespacesForAccount(ctx context.Context, accountID string) (namespaces []*nodepb.Namespace, err error)
 	ListPermissionsInNamespace(ctx context.Context, namespace string) (permissions []*nodepb.Permission, err error)
