8fd80071b3fe012821c8d7329b72e08055656420
Mikita Iwanowski <info@slnt-opp.xyz>
2020-08-14 04:57:49 +0200

    just for -lulz- test

--- pkg/shadow/shadowpb/shadow.pb.go
+++ pkg/shadow/shadowpb/shadow.pb.go
@@ -22,7 +22,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type VersionedValue struct {
 	Version              uint64               `protobuf:"varint,1,opt,name=version,proto3" json:"version,omitempty"`
