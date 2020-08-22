ba3e84b23ae2cb26c25650a41e863e4d8e9a9df1
Preeti Maurya <pritimmaurya@gmail.com>
2020-08-22 18:13:58 +0200

    property recieve maximum added

--- vendor/github.com/infinimesh/mqtt-go/packet/connect.go
+++ vendor/github.com/infinimesh/mqtt-go/packet/connect.go
@@ -24,13 +24,30 @@ import (
 	"io"
 )
 
+const (
+	RecieveMaximumID      = 33
+	MaximumPacketSizeID   = 39
+	TopicAliasMaximumID   = 34
+	RequestResponseInfoID = 25
+	RequestProblemInfoID  = 23
+)
+
+type ConnectProperties struct {
+	PropertyLength         int //variable header properties length
+	RecieveMaximumValue    int //limits the number of QoS 1 and QoS 2 Pub at Client - default 65,535
+	MaximumPacketSize      int //represents max packet size client accepts
+	TopicAliasMaximumValue int //max num of topic alias accepted by client
+	RequestResponseInfo    int //0 = no response info in CONNACK
+	RequestProblemInfo     int //0 = no reason string in CONNACK
+}
+
 type ConnectFlags struct {
-	UserName     bool
-	Password     bool
-	WillRetain   bool
-	WillQoS      int // 2 bytes actually
-	WillFlag     bool
-	CleanSession bool
+	UserName   bool
+	Password   bool
+	WillRetain bool
+	WillQoS    int // 2 bytes actually
+	WillFlag   bool
+	CleanStart bool
 }
 
 type ConnectControlPacket struct {
@@ -40,10 +57,11 @@ type ConnectControlPacket struct {
 }
 
 type ConnectVariableHeader struct {
-	ProtocolName  string
-	ProtocolLevel byte
-	ConnectFlags  ConnectFlags
-	KeepAlive     int
+	ProtocolName      string
+	ProtocolLevel     byte
+	ConnectFlags      ConnectFlags
+	KeepAlive         int
+	ConnectProperties ConnectProperties
 }
 
 type ConnectPayload struct {
@@ -87,7 +105,7 @@ func getConnectVariableHeader(r io.Reader) (hdr ConnectVariableHeader, len int, 
 	hdr.ConnectFlags.Password = connectFlagsByte[0]&64 == 1
 	hdr.ConnectFlags.WillRetain = connectFlagsByte[0]&32 == 1
 	hdr.ConnectFlags.WillFlag = connectFlagsByte[0]&4 == 1
-	hdr.ConnectFlags.CleanSession = connectFlagsByte[0]&2 == 1
+	hdr.ConnectFlags.CleanStart = connectFlagsByte[0]&2 == 1
 
 	keepAliveByte := make([]byte, 2)
 	n, err = r.Read(keepAliveByte)
@@ -110,6 +128,15 @@ func getConnectVariableHeader(r io.Reader) (hdr ConnectVariableHeader, len int, 
 		hdr.ConnectFlags.WillQoS = 1
 	}
 
+	connectRecieveMaximum := make([]byte, 2)
+	n, err = r.Read(connectRecieveMaximum)
+	len += n
+	if err != nil {
+		return hdr, len, errors.New("Could not read recieve maximum byte")
+	}
+	if n > 0 {
+		hdr.ConnectProperties.RecieveMaximumValue = int(binary.BigEndian.Uint16(connectRecieveMaximum))
+	}
 	return
 }
 
