c3a6514b5ab69602dc05960e89484217bf37c0ec
Preeti Maurya <pritimmaurya@gmail.com>
2020-09-07 12:31:17 +0200

    reading new offset

--- cmd/shadow-delta-merger/main.go
+++ cmd/shadow-delta-merger/main.go
@@ -55,7 +55,7 @@ func runMerger(inputTopic, outputTopic, realDeltaTopic, consumerGroup string, st
 	consumerGroupClient := sarama.NewConfig()
 	consumerGroupClient.Version = sarama.V1_0_0_0
 	consumerGroupClient.Consumer.Return.Errors = true
-	consumerGroupClient.Consumer.Offsets.Initial = sarama.OffsetOldest
+	consumerGroupClient.Consumer.Offsets.Initial = sarama.OffsetNewest
 
 	client, err := sarama.NewClient([]string{broker}, consumerGroupClient)
 	fmt.Printf("client created %v\n", client)
