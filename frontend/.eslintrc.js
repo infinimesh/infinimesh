7a36edcd685d471dd2b2d4177b6a8543176fe18b
Mikita Iwanowski <info@slnt-opp.xyz>
2020-09-22 16:31:16 +0200

    eslint conf

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/essential"
+    "plugin:vue/essential",
+    "vuetify"
   ],
   // required to lint *.vue files
   plugins: ["vue"],
