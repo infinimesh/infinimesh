496a42c63d74bf47b2258a071ab034421a6f27c9
Mikita Iwanowski <info@slnt-opp.xyz>
2020-10-29 14:21:43 +0100

    Accounts mobile grid

--- frontend/pages/dashboard/accounts/index.vue
+++ frontend/pages/dashboard/accounts/index.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="accountsTable">
-    <a-row type="flex" align="middle">
-      <a-col :span="12" :offset="1">
-        <h1 class="lead">Accounts</h1>
-      </a-col>
-      <a-col :span="3" :offset="6">
-        <a-row type="flex" justify="end">
-          <a-button
-            type="primary"
-            icon="plus"
-            @click="createAccountDrawerVisible = true"
-            >Create Account</a-button
-          >
+    <a-row>
+      <a-col :span="21" :offset="1">
+        <a-row type="flex" align="middle" justify="space-between">
+          <a-col>
+            <h1 class="lead">Accounts</h1>
+          </a-col>
+          <a-col>
+            <a-row type="flex" justify="end">
+              <a-button
+                type="primary"
+                icon="plus"
+                @click="createAccountDrawerVisible = true"
+                >Create Account</a-button
+              >
+            </a-row>
+            <account-add
+              :active="createAccountDrawerVisible"
+              @cancel="createAccountDrawerVisible = false"
+              @add="handleAccountAdd"
+            />
+          </a-col>
         </a-row>
-        <account-add
-          :active="createAccountDrawerVisible"
-          @cancel="createAccountDrawerVisible = false"
-          @add="handleAccountAdd"
-        />
       </a-col>
     </a-row>
-    <a-row>
+    <a-row style="margin-top: 10px">
       <a-col :span="21" :offset="1">
         <a-table
           :columns="columns"
