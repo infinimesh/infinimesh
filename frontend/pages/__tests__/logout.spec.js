624b1b19ebcbb2789fa6ec89f11c9f0edd64ffc6
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-03 13:30:59 +0100

    logout

--- frontend/pages/__tests__/logout.spec.js
+++ frontend/pages/__tests__/logout.spec.js
@@ -1,24 +1,18 @@
 import { shallowMount } from "@vue/test-utils";
-import Login from "../login";
+import Logout from "../logout";
 
 import Vue from "vue";
 import Antd from "ant-design-vue";
 import vuetify from "vuetify";
 
-describe("Login", () => {
-  let wrapper;
-  beforeEach(() => {
-    Vue.use(Antd);
-    Vue.use(vuetify);
-
-    wrapper = shallowMount(Login);
-  });
+describe("Logout", () => {
+  let wrapper = shallowMount(Logout);
 
   it("mounts properly", () => {
     expect(wrapper.vm).toBeTruthy();
   });
 
   it("renders properly", () => {
-    expect(wrapper.html()).toContain("<h1>infinimesh</h1>");
+    expect(wrapper.html()).toBe("<h1>Logging out...</h1>");
   });
 });
