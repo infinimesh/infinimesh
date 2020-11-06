673e14ad78ce1b0fb578c33c6193e5f07e906034
Preeti Maurya <pritimmaurya@gmail.com>
2020-11-06 15:07:09 +0100

    changed content type

--- cmd/shadow-api/api.go
+++ cmd/shadow-api/api.go
@@ -185,7 +185,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
