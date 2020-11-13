6e6501ff73279cea2400a74edc34cd02229be04e
Preeti Maurya <pritimmaurya@gmail.com>
2020-11-13 14:51:24 +0100

    added logging

--- cmd/shadow-api/api.go
+++ cmd/shadow-api/api.go
@@ -31,6 +31,7 @@ import (
 	"github.com/cskr/pubsub"
 	"github.com/julienschmidt/httprouter"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -92,7 +93,7 @@ func subscribe(consumer sarama.Consumer, ps *pubsub.PubSub, topic, subPath strin
 					fmt.Printf("Invalid message on topic"+topic+" at offset %v, err=%v\n", message.Offset, err)
 					continue
 				}
-
+				log.Println("Publish message to", zap.String(string(message.Key), subPath), zap.String(string(message.Key), string(message.Value)))
 				ps.Pub(&deltaMsg, string(message.Key)+subPath)
 
 				d := DeviceState(deltaMsg.State)
