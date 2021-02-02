76ffb73df3fe21372a0b6bef93cabe3ca9c380da
Ankit Trivedi <ankit_trivedi_15@yahoo.com>
2021-02-02 14:49:26 +0100

    Changed unacceptable changes by Ayesha.

--- pkg/node/dgraph/account.go
+++ pkg/node/dgraph/account.go
@@ -264,18 +264,12 @@ func (s *DGraphRepo) CreateUserAccount(ctx context.Context, username, password s
 		if err != nil {
 			return "", err
 		}
-
-		var uid string
-		if username == "root" && isRoot == true && isAdmin == true {
-			uid = "0x2"
-		} else {
-			uid = "_:user"
-		}
+		
 		//Build the json data structure to create the user for DGraph
 		js, err := json.Marshal(&Account{
 			Node: Node{
 				Type: "account",
-				UID:  uid,
+				UID:  _:user,
 			},
 			Name:    username,
 			IsRoot:  isRoot,
