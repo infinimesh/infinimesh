05d6f585055080cca8bec775baa066bf862c71e8
Mik <info@slnt-opp.xyz>
2021-11-24 16:27:23 +0100

    device-id wildcard

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -33,17 +33,13 @@ func New(fallback string) *Router {
 }
 
 func (r *Router) Route(inputTopic, inputDevice string) (outputTopic string) {
-	fmt.Println("r")
 	splt := strings.Split(inputTopic, "/")
 	switch splt[0] {
 	case "devices":
 		// Check if at least the segment for the deviceID is given plus at least one subtopic segment
 		if len(splt) >= 3 {
 			deviceInTopic := splt[1]
-			if inputDevice != deviceInTopic {
-				// TODO Currently, reject these. However, this will
-				// not be an error anymore once devices can be
-				// authorized to send on behalf of other devices
+			if inputDevice != deviceInTopic && deviceInTopic != "#" {
 				fmt.Println("Input topic does not match device.", deviceInTopic, inputDevice)
 			} else {
 				subtopic := strings.Join(splt[2:], "/")
