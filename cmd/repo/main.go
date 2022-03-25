ec7092caec7b4cc9e0507e19362c8f76ca35e94a
Mik <info@slnt-opp.xyz>
2022-03-25 15:25:50 +0100

    auth set context

--- cmd/repo/main.go
+++ cmd/repo/main.go
@@ -73,6 +73,7 @@ func main() {
 		log.Fatal("Failed to listen", zap.String("address", port), zap.Error(err))
 	}
 
+	auth.SetContext(log, SIGNING_KEY)
 	s := grpc.NewServer(
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpc_zap.UnaryServerInterceptor(log),
