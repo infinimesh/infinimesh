90fbed6984fc9884aa2a44ef832aaa3032b35f82
Mik <info@slnt-opp.xyz>
2022-03-27 18:31:01 +0200

    LICENSE

--- cmd/inf/cmd/accounts.go
+++ cmd/inf/cmd/accounts.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021-2022 Nikita Ivanovski info@slnt-opp.xyz
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
