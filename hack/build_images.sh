aaf208b377526828e0f31bbbc794d9ba53c958cc
Mik <info@slnt-opp.xyz>
2022-03-28 14:20:21 +0200

    Docker:  - update repo url  - cleanup  - repo Dockerfile  - Sample Docker Compose config

--- hack/build_images.sh
+++ hack/build_images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for image in Dockerfiles/*; do
-    tag="ghcr.io/slntopp/infinimesh/$(basename $image):latest"
+    tag="ghcr.io/infinimesh/infinimesh/$(basename $image):latest"
     docker build . -f "$image/Dockerfile" -t $tag
 done
