6146eb4b6d832179349020ca1ea350362a38e0bd
Mik <info@slnt-opp.xyz>
2022-04-01 15:36:37 +0200

    NaiveUI; global style

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <router-view />
+  <n-config-provider :theme="theme">
+    <n-global-style />
+    <router-view />
+  </n-config-provider>
 </template>
 
 <script>
-export default {
-  computed: {
-    isLoggedIn() {
-      return false
-    }
-  },
-  created() {
-    console.log(this)
+import { computed } from "vue"
+import {
+  NConfigProvider, NGlobalStyle,
+  darkTheme, useOsTheme } from 'naive-ui'
 
+export default {
+  components: {
+    NConfigProvider, NGlobalStyle
+  },
+  setup() {
+    console.log(darkTheme)
+    const osThemeRef = useOsTheme();
+    return {
+      theme: computed(() => osThemeRef.value === "dark" ? darkTheme : null),
+      osTheme: osThemeRef
+    };
   }
 }
 </script>
--- dashboard/src/views/Login.vue
+++ dashboard/src/views/Login.vue
@@ -1,3 +1,16 @@
 <template>
-<h1>Login Page</h1>
+  <n-space align="center" justify="center" class="fullscreen">
+    <h1>Login Page</h1>
+  </n-space>
 </template>
+
+<script setup>
+import { NSpace } from "naive-ui"
+</script>
+
+<style>
+.fullscreen {
+  height: 99vh !important;
+  width: 99vw !important;
+}
+</style>
