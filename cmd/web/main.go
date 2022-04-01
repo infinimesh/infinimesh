63f443cd94fc8f9b94fae6cc72870b62b6505544
Mik <info@slnt-opp.xyz>
2022-04-01 18:28:45 +0200

    REST API server base

---
+++ cmd/web/main.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2021-2022 Nikita Ivanovski info@slnt-opp.xyz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"net/http"
+	"strings"
+
+	logger "github.com/infinimesh/infinimesh/pkg/log"
+	pb "github.com/infinimesh/infinimesh/pkg/node/proto"
+
+	"github.com/gorilla/handlers"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	log 			*zap.Logger
+	
+	apiserver 		string
+	corsAllowed 	[]string
+	secure 				bool
+	with_block 		bool
+)
+
+func init() {
+	viper.AutomaticEnv()
+	Log, err := logger.NewProdOrDev()
+	if err != nil {
+		panic(err)
+	}
+	log = Log
+
+	viper.SetDefault("CORS_ALLOWED", []string{"*"})
+	viper.SetDefault("APISERVER_HOST", "proxy:8080")
+	viper.SetDefault("SECURE", false)
+	viper.SetDefault("WITH_BLOCK", false)
+
+	apiserver   = viper.GetString("APISERVER_HOST")
+	corsAllowedIn := viper.GetString("CORS_ALLOWED")
+	if corsAllowedIn != "" {
+		corsAllowed = strings.Split(corsAllowedIn, ",")
+	}
+	secure      = viper.GetBool("SECURE")
+	with_block  = viper.GetBool("WITH_BLOCK")
+}
+
+func main() {
+	defer func() {
+		_ = log.Sync()
+	}()
+
+	log.Info("Starting REST-API Server")
+	log.Info("Registering Endpoints", zap.String("server", apiserver))
+	var err error
+
+	gwmux := runtime.NewServeMux()
+	creds := insecure.NewCredentials()
+	if secure {
+		creds = credentials.NewTLS(&tls.Config{InsecureSkipVerify: true})
+	}
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+
+	if with_block {
+		opts = append(opts, grpc.WithBlock())
+	}
+
+	log.Info("Registering Accounts Service")
+	err = pb.RegisterAccountsServiceHandlerFromEndpoint(context.Background(), gwmux, apiserver, opts)
+	if err != nil {
+		log.Fatal("Failed to register AccountsService gateway", zap.Error(err))
+	}
+
+	log.Info("Allowed Origins", zap.Strings("hosts", corsAllowed))
+	handler := handlers.CORS(
+		handlers.AllowedOrigins(corsAllowed),
+		handlers.AllowedHeaders([]string{"Content-Type"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS", "HEAD"}),
+	)(gwmux)
+
+	log.Info("Serving gRPC-Gateway on http://0.0.0.0:8000")
+	log.Fatal("Failed to Listen and Serve Gateway-Server", zap.Error(http.ListenAndServe(":8000", handler)))
+}
--- go.mod
+++ go.mod
@@ -40,9 +40,12 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
