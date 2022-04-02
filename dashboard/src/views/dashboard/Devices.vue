d72e51a23b5d673eb044b1da493fcf9b327c0857
Mik <info@slnt-opp.xyz>
2022-04-02 18:07:24 +0200

    load Devices setup

--- dashboard/src/views/dashboard/Devices.vue
+++ dashboard/src/views/dashboard/Devices.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>I am Devices Dashboard</h1>
 </template>
+
+<script setup>
+import { ref } from "vue"
+import { useAppStore } from "@/store/app";
+
+const loading = ref(false)
+
+const axios = useAppStore().http
+async function loadDevices() {
+  loading.value = true
+  axios.get('http://localhost:8000/devices', {}).then(res => {
+    console.log(res.data)
+    loading.value = false
+  })
+}
+
+loadDevices()
+</script>
