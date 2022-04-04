cb3636442d75a673a65c0148f5aeaeb66fd7b117
Mik <info@slnt-opp.xyz>
2022-04-04 19:34:49 +0200

    Messages Provider

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <n-config-provider :theme="theme" :hljs="hljs">
-    <n-global-style />
-    <router-view />
-    <n-watermark
-      v-if="watermark"
-      content="development preview"
-      cross
-      fullscreen
-      :font-size="16"
-      :line-height="16"
-      :width="250"
-      :height="150"
-      :x-offset="12"
-      :y-offset="80"
-      :rotate="-15"
-    />
+    <n-message-provider>
+      <n-global-style />
+      <router-view />
+      <n-watermark
+        v-if="watermark"
+        content="development preview"
+        cross
+        fullscreen
+        :font-size="16"
+        :line-height="16"
+        :width="250"
+        :height="150"
+        :x-offset="12"
+        :y-offset="80"
+        :rotate="-15"
+      />
+    </n-message-provider>
   </n-config-provider>
 </template>
 
@@ -22,13 +24,13 @@
 import { computed } from "vue"
 import {
   NConfigProvider, NGlobalStyle, NWatermark,
-  darkTheme, useOsTheme } from 'naive-ui'
+  NMessageProvider, darkTheme, useOsTheme } from 'naive-ui'
 
 import hljs from "@/utils/hljs"
 
 export default {
   components: {
-    NConfigProvider, NGlobalStyle, NWatermark
+    NConfigProvider, NGlobalStyle, NWatermark, NMessageProvider,
   },
   setup() {
     const osThemeRef = useOsTheme();
