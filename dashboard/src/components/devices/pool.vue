c72cdea03d837b00c08c25972c066930b41f8bcb
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-04 00:24:02 +0200

    Devices table nav responsivness

--- dashboard/src/components/devices/pool.vue
+++ dashboard/src/components/devices/pool.vue
@@ -1,18 +1,14 @@
 <template>
-  <n-empty
-    size="huge"
-    description="No Devices"
-    v-if="devices.length == 0"
-  ></n-empty>
+  <n-empty size="huge" description="No Devices" v-if="devices.length == 0"></n-empty>
   <div v-else>
-    <n-grid responsive="screen">
-      <n-grid-item span="3">
+    <n-grid item-responsive y-gap="10">
+      <n-grid-item span="24 500:8 600:6 1000:4">
         <n-h1 prefix="bar" align-text type="info">
           <n-text type="info"> Devices </n-text>
         </n-h1>
       </n-grid-item>
-      <n-grid-item span="15"> </n-grid-item>
-      <n-grid-item span="3">
+      <n-grid-item span="0 600:2 700:4 1000:12 1400:14"> </n-grid-item>
+      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:3">
         <n-button strong secondary round type="info" @click="emit('refresh')">
           <template #icon>
             <n-icon>
@@ -22,18 +18,13 @@
           Refresh State
         </n-button>
       </n-grid-item>
-      <n-grid-item span="3">
+      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:2">
         <device-create />
       </n-grid-item>
     </n-grid>
-    <n-grid cols="1 s:1 m:2 l:3 xl:4 2xl:4" ref="grid" responsive="screen">
+    <n-grid cols="1 s:1 m:2 l:3 xl:4 2xl:4" ref="grid" responsive="screen" style="margin-top: 10px">
       <n-grid-item v-for="(col, i) in pool" :key="i">
-        <device-card
-          v-for="device in col"
-          :key="device.uuid"
-          :device="device"
-          :show_ns="show_ns"
-        />
+        <device-card v-for="device in col" :key="device.uuid" :device="device" :show_ns="show_ns" />
       </n-grid-item>
     </n-grid>
   </div>
