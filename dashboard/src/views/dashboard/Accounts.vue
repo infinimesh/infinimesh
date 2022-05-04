d924e369910adccc1e5d61c8111bbc7fdce9c864
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-04 05:24:22 +0200

    Manage owned accounts credentials

--- dashboard/src/views/dashboard/Accounts.vue
+++ dashboard/src/views/dashboard/Accounts.vue
@@ -69,7 +69,7 @@
             <n-space>
               <n-tooltip trigger="hover">
                 <template #trigger>
-                  <n-button tertiary round :type="account.enabled ? 'error' : 'success'"
+                  <n-button tertiary circle :type="account.enabled ? 'error' : 'success'"
                     @click="e => handleToggleAccountEnabled(account)">
                     <template #icon>
                       <n-icon>
@@ -79,13 +79,29 @@
                     </template>
                   </n-button>
                 </template>
-                Click to {{ account.enabled ? "disable" : "enable" }} Account
+                <span>Click to {{ account.enabled ? "disable" : "enable" }} <b>{{ account.title }}'s</b> Account</span>
               </n-tooltip>
+
+              <n-tooltip v-if="access_lvl_conv(account) > 3 || account.access.role == 'OWNER'" trigger="hover">
+                <template #trigger>
+                  <n-button type="warning" @click="() => { active_account = account; show_mc = true }" tertiary circle>
+                    <template #icon>
+                      <n-icon>
+                        <lock-closed-outline />
+                      </n-icon>
+                    </template>
+                  </n-button>
+                </template>
+                <span>Click to manage <b>{{ account.title }}'s</b> credentials</span>
+              </n-tooltip>
+
               <n-popconfirm @positive-click="() => handleDelete(account.uuid)">
                 <template #trigger>
                   <n-button v-if="access_lvl_conv(account) > 2" type="error" round secondary>Delete</n-button>
                 </template>
-                Are you sure about deleting this account?
+                <span>
+                  Are you sure about deleting <b>{{ account.title }}'s</b> account?
+                </span>
               </n-popconfirm>
             </n-space>
           </td>
@@ -93,10 +109,12 @@
       </tbody>
     </n-table>
   </n-spin>
+
+  <set-credentials-modal :show="show_mc" @close="show_mc = false" :account="active_account" />
 </template>
 
 <script setup>
-import { h, computed } from "vue";
+import { h, ref } from "vue";
 import {
   NSpin,
   NTable,
@@ -111,16 +129,15 @@ import {
   NH1,
   NText, useLoadingBar
 } from "naive-ui";
-import { CopyOutline, CheckmarkOutline, BanOutline, RefreshOutline } from "@vicons/ionicons5";
+import { CopyOutline, CheckmarkOutline, BanOutline, RefreshOutline, LockClosedOutline } from "@vicons/ionicons5";
 import { useAccountsStore } from "@/store/accounts";
 import { storeToRefs } from "pinia";
 import AccountCreate from "@/components/accounts/create-drawer.vue";
 import { access_lvl_conv } from "@/utils/access";
+import setCredentialsModal from "@/components/accounts/set-credentials-modal.vue";
 
 const store = useAccountsStore();
 const { accounts_ns_filtered: accounts, loading } = storeToRefs(store);
-
-const pool = computed(() => accounts);
 
 store.fetchAccounts();
 
@@ -183,4 +200,7 @@ function handleDelete(uuid) {
 function handleToggleAccountEnabled(account) {
   store.toggle(account.uuid, bar);
 }
+
+const show_mc = ref(false);
+const active_account = ref({})
 </script>
