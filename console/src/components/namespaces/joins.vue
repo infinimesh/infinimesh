db08bec404676ec13d1d5e1bf1343231126f27f0
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-07 15:56:48 +0200

    add refresh joins button

--- console/src/components/namespaces/joins.vue
+++ console/src/components/namespaces/joins.vue
@@ -1,10 +1,25 @@
 <template>
     <n-tr>
         <n-td colspan="1"></n-td>
-        <n-td colspan="4">
+        <n-td colspan="1">
             <n-text type="warning" strong>
                 List of Accounts, who has access to this Namespace
             </n-text>
+        </n-td>
+        <n-td colspan="2">
+            <n-text type="warning" strong v-if="!loading && joins.length == 0">
+                No Accounts found
+            </n-text>
+        </n-td>
+        <n-td>
+            <n-button strong quaternary round type="info" @click="load">
+                <template #icon>
+                    <n-icon>
+                        <refresh-outline />
+                    </n-icon>
+                </template>
+                Refresh
+            </n-button>
         </n-td>
     </n-tr>
     <n-tr v-if="loading">
@@ -32,7 +47,8 @@
 
 <script setup>
 import { ref, onMounted } from "vue"
-import { NTr, NTd, NProgress, NText } from "naive-ui"
+import { NTr, NTd, NProgress, NText, NButton, NIcon } from "naive-ui"
+import { RefreshOutline } from "@vicons/ionicons5";
 
 import UuidBadge from "@/components/core/uuid-badge.vue";
 import AccessBadge from "@/components/core/access-badge"
