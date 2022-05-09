56950397bc69900016fdd883f7dc75d08e64e90f
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-09 13:35:59 +0200

    move Devies page header to the page itself

--- console/src/components/devices/pool.vue
+++ console/src/components/devices/pool.vue
@@ -1,27 +1,6 @@
 <template>
   <n-empty size="huge" description="No Devices" v-if="devices.length == 0"></n-empty>
   <div v-else>
-    <n-grid item-responsive y-gap="10">
-      <n-grid-item span="24 500:8 600:6 1000:4">
-        <n-h1 prefix="bar" align-text type="info">
-          <n-text type="info"> Devices </n-text>
-        </n-h1>
-      </n-grid-item>
-      <n-grid-item span="0 600:2 700:4 1000:12 1400:14"> </n-grid-item>
-      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:3">
-        <n-button strong secondary round type="info" @click="emit('refresh')">
-          <template #icon>
-            <n-icon>
-              <refresh-outline />
-            </n-icon>
-          </template>
-          Refresh State
-        </n-button>
-      </n-grid-item>
-      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:2">
-        <device-create />
-      </n-grid-item>
-    </n-grid>
     <n-grid cols="1 s:1 m:2 l:3 xl:4 2xl:4" ref="grid" responsive="screen" style="margin-top: 10px">
       <n-grid-item v-for="(col, i) in pool" :key="i">
         <device-card v-for="device in col" :key="device.uuid" :device="device" :show_ns="show_ns" />
@@ -32,10 +11,8 @@
 
 <script setup>
 import { ref, computed } from "vue";
-import { NEmpty, NGrid, NGridItem, NH1, NText, NIcon, NButton } from "naive-ui";
-import { RefreshOutline } from "@vicons/ionicons5";
+import { NEmpty, NGrid, NGridItem, } from "naive-ui";
 import DeviceCard from "./device-card.vue";
-import DeviceCreate from "./create-drawer.vue";
 
 const grid = ref({ responsiveCols: 0 });
 
--- console/src/views/dashboard/Devices.vue
+++ console/src/views/dashboard/Devices.vue
@@ -1,18 +1,37 @@
 <template>
   <n-spin :show="loading">
-    <devices-pool
-      :devices="devices"
-      :show_ns="show_ns"
-      @refresh="() => store.fetchDevices()"
-    />
+    <n-grid item-responsive y-gap="10">
+      <n-grid-item span="24 500:8 600:6 1000:4">
+        <n-h1 prefix="bar" align-text type="info">
+          <n-text type="info"> Devices </n-text>
+        </n-h1>
+      </n-grid-item>
+      <n-grid-item span="0 600:2 700:4 1000:12 1400:14"> </n-grid-item>
+      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:3">
+        <n-button strong secondary round type="info" @click="emit('refresh')">
+          <template #icon>
+            <n-icon>
+              <refresh-outline />
+            </n-icon>
+          </template>
+          Refresh State
+        </n-button>
+      </n-grid-item>
+      <n-grid-item span="24 300:12 500:7 600:6 700:5 1000:4 1400:2">
+        <device-create />
+      </n-grid-item>
+    </n-grid>
+    <devices-pool :devices="devices" :show_ns="show_ns" @refresh="() => store.fetchDevices()" />
   </n-spin>
 </template>
 
 <script setup>
-import { NSpin } from "naive-ui";
+import { NSpin, NH1, NText, NIcon, NButton, NGrid, NGridItem } from "naive-ui";
+import { RefreshOutline } from "@vicons/ionicons5";
 import { useDevicesStore } from "@/store/devices";
 import { storeToRefs } from "pinia";
 import DevicesPool from "@/components/devices/pool.vue";
+import DeviceCreate from "@/components/devices/create-drawer.vue";
 
 const store = useDevicesStore();
 const { loading, devices_ns_filtered: devices, show_ns } = storeToRefs(store);
