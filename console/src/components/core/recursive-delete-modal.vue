e22d840d9990b33f57ec998e140b47c7e981c294
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-09 19:02:17 +0200

    recursive delete modal

---
+++ console/src/components/core/recursive-delete-modal.vue
@@ -0,0 +1,123 @@
+<template>
+    <n-popconfirm @positive-click="() => show = true">
+        <template #trigger>
+            <n-button v-if="o.access.role == 'OWNER'" type="error" round secondary @click.stop.prevent>Delete
+            </n-button>
+        </template>
+        <span>
+            Are you sure about deleting {{ type }} <b>{{ o.title }}</b>?
+        </span>
+    </n-popconfirm>
+    <n-modal v-model:show="show">
+        <n-spin :show="loading">
+            <n-card style="width: 600px" title="To be deleted" :bordered="false" size="huge" role="dialog"
+                aria-modal="true">
+                <n-tree :data="tree" :default-expand-all="true" />
+                <template #footer>
+                    <n-space justify="end">
+                        <n-button type="info" round secondary @click="show = false">
+                            Cancel
+                        </n-button>
+                        <n-button type="error" round secondary @click="() => { emit('confirm'); show = false }">
+                            Confirm Delete
+                        </n-button>
+                    </n-space>
+                </template>
+            </n-card>
+        </n-spin>
+    </n-modal>
+</template>
+
+<script setup>
+import { h, ref, watch } from "vue"
+
+import { NPopconfirm, NModal, NCard, NButton, NSpin, NTree, NSpace } from "naive-ui";
+import { useAccountsStore } from "@/store/accounts";
+import { useNSStore } from "@/store/namespaces";
+import { useDevicesStore } from "@/store/devices";
+
+const accs = useAccountsStore();
+const nss = useNSStore()
+const devs = useDevicesStore();
+
+const show = ref(false)
+const loading = ref(true)
+const tree = ref([])
+
+const { o, type, deletables } = defineProps({
+    o: {
+        type: Object,
+        required: true
+    },
+    deletables: {
+        default: () => () => [],
+    },
+    type: {
+        type: String,
+        default: "namespace"
+    }
+})
+
+const emit = defineEmits(['confirm'])
+
+watch(show, async (val) => {
+    if (val) {
+        loading.value = true
+        const { data } = await deletables()
+
+        if (!Object.keys(accs.accounts).length) {
+            accs.fetchAccounts()
+        }
+        if (!Object.keys(devs.devices).length) {
+            devs.fetchDevices()
+        }
+
+        tree.value = makeTree(data.nodes)
+        loading.value = false
+    }
+})
+
+function makeTree(data, parent = '') {
+    let nodes = []
+    data = data.filter((value) => {
+        if (value.parent == parent) {
+            nodes.push(value)
+            return false
+        }
+        return true
+    })
+
+    return nodes.map(node => {
+        return {
+            key: node.node,
+            label: () => resolve(node.node),
+            children: makeTree(data, node.node)
+        }
+    })
+}
+
+function resolve(node) {
+    const [id, uuid] = node.split('/')
+    let type, title;
+    switch (id) {
+        case "Accounts":
+            type = "Account"
+            title = accs.accounts[uuid]?.title
+            break
+        case "Namespaces":
+            type = "Namespace"
+            title = nss.namespaces[uuid]?.title
+            break
+        case "Devices":
+            type = "Device"
+            title = devs.devices[uuid]?.title
+            break
+        default:
+            type = id.slice(0, id.length)
+            title = ""
+    }
+
+    return h('span', [type + " ", h('b', title), `(${uuid})`])
+}
+
+</script>
