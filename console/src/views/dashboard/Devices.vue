fc7c16b41411208a281858e2fcfaecdad118e2f2
Mik <info@slnt-opp.xyz>
2022-07-12 23:17:01 +0200

    responsivness with extra button

--- console/src/views/dashboard/Devices.vue
+++ console/src/views/dashboard/Devices.vue
@@ -1,13 +1,11 @@
 <template>
   <n-spin :show="loading">
-    <n-grid item-responsive y-gap="10">
-      <n-grid-item span="24 500:8 600:4 1000:4">
-        <n-h1 prefix="bar" align-text type="info">
-          <n-text type="info"> Devices </n-text>
-        </n-h1>
-      </n-grid-item>
-      <n-grid-item span="24 300:24 500:14 600:20 1000:20">
-        <n-space justify="end" align="center">
+    <n-grid item-responsive y-gap="10" x-gap="10">
+      <n-grid-item span="24 500:14 600:12 1000:12">
+        <n-space justify="space-between">
+          <n-h1 prefix="bar" align-text type="info">
+            <n-text type="info"> Devices </n-text>
+          </n-h1>
           <n-button strong secondary round type="info" @click="handleRefresh">
             <template #icon>
               <n-icon>
@@ -16,8 +14,12 @@
             </template>
             Refresh State
           </n-button>
+        </n-space>
+      </n-grid-item>
+      <n-grid-item span="24 300:24 500:10 600:12 1000:12">
+        <n-space justify="space-evenly" align="center">
           <device-create />
-          <device-register />
+          <device-register v-if="console_services.handsfree != undefined" />
         </n-space>
       </n-grid-item>
     </n-grid>
@@ -28,8 +30,11 @@
 <script setup>
 import { NSpin, NH1, NText, NIcon, NButton, NGrid, NGridItem, NSpace } from "naive-ui";
 import { RefreshOutline } from "@vicons/ionicons5";
+
+import { useAppStore } from "@/store/app";
 import { useDevicesStore } from "@/store/devices";
 import { storeToRefs } from "pinia";
+
 import DevicesPool from "@/components/devices/pool.vue";
 import DeviceCreate from "@/components/devices/create-drawer.vue";
 import DeviceRegister from "@/components/devices/register-modal.vue";
@@ -38,6 +43,8 @@ const store = useDevicesStore();
 const { loading, devices_ns_filtered: devices, show_ns } = storeToRefs(store);
 
 store.fetchDevices(true, true);
+
+const { console_services } = storeToRefs(useAppStore())
 
 function handleRefresh() {
   store.fetchDevices(true);
