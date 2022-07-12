858c0431d2f4f936f34047c96a3d35c49586dcd1
Mik <info@slnt-opp.xyz>
2022-07-12 18:59:02 +0200

    plugins state router

--- cmd/shadow/shadow.go
+++ cmd/shadow/shadow.go
@@ -16,6 +16,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 
@@ -24,10 +25,17 @@ import (
 	"github.com/streadway/amqp"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 
+	"github.com/infinimesh/infinimesh/pkg/graph/schema"
 	logger "github.com/infinimesh/infinimesh/pkg/log"
 	"github.com/infinimesh/infinimesh/pkg/mqtt/pubsub"
 	"github.com/infinimesh/infinimesh/pkg/shadow"
+	"github.com/infinimesh/infinimesh/pkg/shadow/plugins"
+	"github.com/infinimesh/infinimesh/pkg/shared/auth"
+	nodepb "github.com/infinimesh/proto/node"
+	devpb "github.com/infinimesh/proto/node/devices"
 	pb "github.com/infinimesh/proto/shadow"
 )
 
@@ -36,7 +44,9 @@ var (
 
 	port         string
 	redisHost    string
+	devicesHost  string
 	RabbitMQConn string
+	SIGNING_KEY  string
 )
 
 func init() {
@@ -46,11 +56,15 @@ func init() {
 
 	viper.SetDefault("PORT", "8000")
 	viper.SetDefault("REDIS_HOST", "redis:6379")
+	viper.SetDefault("DEVICES_HOST", "redis:6379")
 	viper.SetDefault("RABBITMQ_CONN", "amqp://infinimesh:infinimesh@rabbitmq:5672/")
+	viper.SetDefault("SIGNING_KEY", "seeeecreet")
 
 	port = viper.GetString("PORT")
 	redisHost = viper.GetString("REDIS_HOST")
+	devicesHost = viper.GetString("DEVICES_HOST")
 	RabbitMQConn = viper.GetString("RABBITMQ_CONN")
+	SIGNING_KEY = viper.GetString("SIGNING_KEY")
 }
 
 func main() {
@@ -79,6 +93,31 @@ func main() {
 		log.Fatal("Error setting up pubsub", zap.Error(err))
 	}
 	log.Info("Pub/Sub setup complete")
+
+	SIGNING_KEY := []byte(viper.GetString("SIGNING_KEY"))
+	auth.SetContext(log, SIGNING_KEY)
+	token, err := auth.MakeToken(schema.ROOT_ACCOUNT_KEY)
+	if err != nil {
+		log.Fatal("Error making token", zap.Error(err))
+	}
+	internal_ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization", "Bearer "+token)
+
+	log.Info("Connecting to registry", zap.String("host", devicesHost))
+	conn, err := grpc.Dial(devicesHost, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("Error dialing device registry", zap.Error(err))
+	}
+	client := nodepb.NewDevicesServiceClient(conn)
+
+	plugins.Setup(log, rbmq, ps, func(uuid string) *devpb.Device {
+		dev, err := client.Get(internal_ctx, &devpb.Device{
+			Uuid: uuid,
+		})
+		if err != nil {
+			return nil
+		}
+		return dev
+	})
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
 	if err != nil {
---
+++ pkg/shadow/plugins/exchange.go
@@ -0,0 +1,116 @@
+/*
+Copyright © 2021-2022 Infinite Devices GmbH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package plugins
+
+import (
+	"strings"
+
+	"github.com/cskr/pubsub"
+	devpb "github.com/infinimesh/proto/node/devices"
+	pb "github.com/infinimesh/proto/shadow"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+)
+
+var (
+	logger  *zap.Logger
+	channel *amqp.Channel
+	qs      map[string]bool
+)
+
+type FetchDeviceFunc func(string) *devpb.Device
+
+func Setup(Log *zap.Logger, conn *amqp.Connection, ps *pubsub.PubSub, fetcher FetchDeviceFunc) error {
+	logger = Log.Named("PluginsExchange")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	err = ch.ExchangeDeclare("plugins", "topic", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	channel = ch
+	qs = make(map[string]bool)
+
+	go func(messages chan interface{}) {
+		for msg := range messages {
+			logger.Info("Received message to Broadcast")
+			shadow := msg.(*pb.Shadow)
+			dev := fetcher(shadow.GetDevice())
+			if dev == nil {
+				continue
+			}
+
+			Publish(dev, shadow)
+		}
+	}(ps.Sub("mqtt.outgoing", "mqtt.incoming"))
+
+	return nil
+}
+
+func Publish(dev *devpb.Device, state *pb.Shadow) {
+	for _, tag := range dev.Tags {
+		plugin := strings.TrimPrefix(tag, "plugin:")
+		if plugin != tag {
+			PublishSingle(plugin, state)
+		}
+	}
+}
+
+func PublishSingle(plugin string, state *pb.Shadow) {
+	log := logger.Named("Publisher")
+	log.Debug("Publish request received", zap.String("plugin", plugin))
+
+	_, ok := qs[plugin]
+	if !ok {
+		queue, err := channel.QueueDeclare(
+			"plugin."+plugin, false, false, false, false, nil,
+		)
+		if err != nil {
+			log.Warn("Error declaring queue", zap.Error(err))
+			return
+		}
+
+		err = channel.QueueBind(queue.Name, plugin, "plugins", false, nil)
+		if err != nil {
+			log.Warn("Error binding queue", zap.Error(err))
+			return
+		}
+
+		qs[plugin] = true
+		log.Info("Queue declared", zap.String("name", queue.Name))
+	}
+
+	body, err := proto.Marshal(state)
+	if err != nil {
+		log.Warn("Error Marshaling state", zap.Error(err))
+		return
+	}
+	err = channel.Publish("plugins", plugin, false, false, amqp.Publishing{
+		ContentType: "text/plain", Body: body,
+	})
+	if err != nil {
+		log.Warn("Error Publishing message", zap.Error(err))
+		delete(qs, plugin)
+		return
+	}
+	log.Debug("State published")
+}
