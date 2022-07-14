8059db223c7bbafd494aed5dbb06bb973792db0a
Mik <info@slnt-opp.xyz>
2022-07-14 12:03:02 +0200

    image tag version parameter

--- hack/build_images.sh
+++ hack/build_images.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+version="${1:-latest}"
 for image in Dockerfiles/*; do
     tag="ghcr.io/infinimesh/infinimesh/$(basename $image):latest"
     docker build . -f "$image/Dockerfile" -t $tag
