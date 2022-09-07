95ae3b1f8c6b21cc6d86b0bf7f55b63f0d9e4ac1
Mik Iwanowski <info@slnt-opp.xyz>
2022-09-07 13:02:03 +0200

    cleanup

--- hack/patch_desired_state.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export TOKEN=$(curl -s -X POST -d '{"username" : "joe", "password": "test123"}'  localhost:8081/token | jq -r ".token")
-grpcurl -H "authorization: bearer $TOKEN" -plaintext -d "{\"id\" : \"testdevice4\", \"data\" : $RANDOM }" localhost:8000 infinimesh.api.Shadows/PatchDesiredState
--- hack/push_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for image in Dockerfiles/*; do
-    tag="ghcr.io/infinimesh/infinimesh/$(basename $image):latest"
-    docker push $tag
-done
