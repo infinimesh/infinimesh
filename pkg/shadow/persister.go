3b6a7ba91cb117c0fe45f7ecac93bd85cdb53dee
Mik Iwanowski <info@slnt-opp.xyz>
2022-11-18 17:59:46 +0100

    log Persister state

--- pkg/shadow/persister.go
+++ pkg/shadow/persister.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -39,6 +39,10 @@ func Key(device string, key pb.StateKey) string {
 
 func (s *ShadowServiceServer) Persister() {
 	log := s.log.Named("persister")
+
+	log.Info("Starting Persister")
+	defer log.Warn("Exited")
+
 	messages := make(chan interface{}, 10)
 	s.ps.AddSub(messages, "mqtt.incoming", "mqtt.outgoing")
 
