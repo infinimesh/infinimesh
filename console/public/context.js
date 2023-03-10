36a0421f2d0612e31cb546619817bf9882343660
Mik Iwanowski <info@slnt-opp.xyz>
2023-03-10 19:15:12 +0100

    merge theme overrides into single context.js config

---
+++ console/public/context.js
@@ -0,0 +1,17 @@
+const LightThemeOverrides = {
+    "common": {
+        "primaryColor": "#70C0E8FF",
+        "primaryColorSuppl": "#3889C5FF",
+        "primaryColorPressed": "#66AFD3FF",
+        "primaryColorHover": "#8ACBECFF"
+    }
+}
+
+const DarkThemeOverrides = {
+    "common": {
+        "primaryColor": "#70C0E8FF",
+        "primaryColorSuppl": "#3889C5FF",
+        "primaryColorPressed": "#66AFD3FF",
+        "primaryColorHover": "#8ACBECFF"
+    }
+}
--- console/public/theme/light-theme-overrides.js
+++
@@ -1,8 +0,0 @@
-const LightThemeOverrides = {
-  "common": {
-    "primaryColor": "#70C0E8FF",
-    "primaryColorSuppl": "#3889C5FF",
-    "primaryColorPressed": "#66AFD3FF",
-    "primaryColorHover": "#8ACBECFF"
-  }
-}
