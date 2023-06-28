6a6a7ad7bb1e478fbc002e3177588c13f139a832
Mikita Iwanowski <info@slnt-opp.xyz>
2023-06-28 14:01:58 +0200

    Connection State Client ID (#425)
    * handle store last client id
    
    * refactor connection state corner:
     - use table and bigger font
     - use icons
     - add Client ID

--- console/src/components/devices/device_card/status-corner.vue
+++ console/src/components/devices/device_card/status-corner.vue
@@ -3,9 +3,14 @@
 </template>
 
 <script setup>
-import { NTooltip } from 'naive-ui';
-import { h } from 'vue';
+import { NTooltip, NTable, NCode, NIcon, NSpace, useThemeVars } from 'naive-ui';
+import { defineAsyncComponent, h } from 'vue';
 
+const IdCardOutline = defineAsyncComponent(() => import('@vicons/ionicons5/IdCardOutline'))
+const TelescopeOutline = defineAsyncComponent(() => import('@vicons/ionicons5/TelescopeOutline'))
+
+const theme = useThemeVars()
+console.dir(theme.value)
 
 const props = defineProps({
     connection: {
@@ -13,6 +18,15 @@ const props = defineProps({
     }
 })
 
+function tooltip_row({ icon, label, value }) {
+    return h('tr', [
+        h('th', label),
+        h('th',
+            h(NIcon, { component: icon, size: 20, style: { paddingTop: '10px'  }})
+        ),
+        h('td', value)
+    ])
+}
 
 function render() {
 
@@ -31,6 +45,18 @@ function render() {
         }
     }
 
+    let tooltip = [
+        tooltip_row({
+            icon: TelescopeOutline, label: 'Last Seen', value: seen
+        })
+    ]
+
+    if (props.connection.clientId) {
+        tooltip.push(tooltip_row({
+            icon: IdCardOutline, label: 'Client ID', value: h(NCode, props.connection.clientId)
+        }))
+    }
+
     return h(NTooltip, {
         trigger: "hover",
         placement: "bottom",
@@ -38,7 +64,7 @@ function render() {
         trigger: () => h('div', {
             class: ['triangle', status]
         }),
-        default: () => `Last Seen: ${seen}`
+        default: () => h(NTable, { bordered: false, bottomBordered: false }, () => h('tbody', tooltip))
     })
 }
 </script>
