74d67631dd2d618d3479c890fc680b03cf7e8c10
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-02-05 17:28:40 +0300

    Complete main.go file

--- cmd/oauth/main.go
+++ cmd/oauth/main.go
@@ -1,1 +1,71 @@
-package oauth
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/infinimesh/infinimesh/pkg/graph/schema"
+	logger "github.com/infinimesh/infinimesh/pkg/log"
+	"github.com/infinimesh/infinimesh/pkg/oauth"
+	"github.com/infinimesh/infinimesh/pkg/shared/auth"
+	"github.com/infinimesh/proto/node/nodeconnect"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	log *zap.Logger
+
+	port               string
+	accountsConnection string
+	signingKey         []byte
+	corsAllowedIn      string
+	configs            map[string]oauth.Config
+)
+
+func init() {
+	viper.AutomaticEnv()
+
+	log = logger.NewLogger()
+	viper.SetDefault("PORT", "8000")
+	viper.SetDefault("REGISTRY", "repo:8000")
+	viper.SetDefault("SIGNING_KEY", "seeeecreet")
+	viper.SetDefault("CORS_ALLOWED", []string{"*"})
+
+	port = viper.GetString("PORT")
+	accountsConnection = viper.GetString("REGISTRY")
+	signingKey = []byte(viper.GetString("SIGNING_KEY"))
+	corsAllowedIn = viper.GetString("CORS_ALLOWED")
+
+	file, err := os.ReadFile("oauth2_config.yaml")
+	if err != nil {
+		log.Fatal("Failed to read config", zap.Error(err))
+	}
+	err = yaml.Unmarshal(file, &configs)
+	if err != nil {
+		log.Fatal("Failed to parse config", zap.Error(err))
+	}
+}
+
+func main() {
+	defer func() {
+		_ = log.Sync()
+	}()
+
+	router := mux.NewRouter()
+	client := nodeconnect.NewAccountsServiceClient(http.DefaultClient, accountsConnection)
+
+	interceptor := auth.NewAuthInterceptor(log, nil, nil, signingKey)
+	token, err := interceptor.MakeToken(schema.ROOT_ACCOUNT_KEY)
+	if err != nil {
+		log.Fatal("Failed to create token", zap.Error(err))
+	}
+
+	cors := strings.Split(corsAllowedIn, ",")
+
+	service := oauth.NewOauthService(log, router)
+	service.Register(configs, client, token)
+	service.Run(port, cors)
+}
