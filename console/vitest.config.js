0d65391d68f7bf6e743d1102f0c0ab1668c700a5
Pavel Sazukin <97695224+SazukinPavel@users.noreply.github.com>
2024-02-20 14:47:40 +0400

    Dev store tests (#716)
    * update proto
    
    * add dev enviroment
    
    * refactor & cover with tests store/accounts.js
    
    * refactor & cover with tests store/plugins.js
    
    * feat: added tests for devices store
    
    * feat: added tests for namespaces store
    
    * update: proto version
    
    * fix: fixed import of namespaces mocks
    
    ---------
    
    Co-authored-by: Mikita Iwanowski <info@slnt-opp.xyz>
    Co-authored-by: 639852 <sasha.bobyliev@mail.ru>

--- console/vitest.config.js
+++ console/vitest.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "happy-dom",
+    env: { VITE_MOCK: true },
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
