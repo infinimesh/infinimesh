2e16096abaf294c0eca8496823d84d8702a16c79
Pavel Sazukin <sazukinpavelal@gmail.com>
2024-02-27 16:39:33 +0400

    add event bus

---
+++ console/src/utils/event-bus.js
@@ -0,0 +1,35 @@
+export class EventBus {
+  #eventsMap;
+
+  constructor() {
+    this.#eventsMap = new Map();
+  }
+
+  publish(eventName, ...args) {
+    const callbackObject = this.#eventsMap.get(eventName);
+
+    if (!callbackObject) return;
+    for (let id of Object.getOwnPropertySymbols(callbackObject)) {
+      callbackObject[id](...args);
+    }
+  }
+
+  subscribe(eventName, callback) {
+    if (!this.#eventsMap.get(eventName)) {
+      this.#eventsMap.set(eventName, {});
+    }
+
+    const id = Symbol(eventName);
+
+    this.#eventsMap.get(eventName)[id] = callback;
+    const unSubscribe = () => {
+      delete this.#eventsMap.get(eventName)[id];
+
+      if (Object.keys(this.#eventsMap.get(eventName)).length === 0) {
+        this.#eventsMap.delete(eventName);
+      }
+    };
+
+    return { unSubscribe };
+  }
+}
